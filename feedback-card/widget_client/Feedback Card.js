85ff0a3ed3bbd7f466bb5f14f4825266f0c8b301
fabio.petito <fabio.petito@servicenow.com>
2017-02-27 16:04:43 -0800

    Feedback Card widget

--- feedback-card/widget_client/Feedback Card.js
+++ feedback-card/widget_client/Feedback Card.js
@@ -1,4 +1,28 @@
-function() {
-  /* widget controller */
+function($timeout, $rootScope) {
   var c = this;
+  
+  c.showSurvey = true;
+  c.step = 'one';
+  c.goodrandom = Math.floor(Math.random() * 21) + 80;
+  c.badrandom = Math.floor(Math.random() * 6) + 1;
+  
+  c.$onInit = function () {
+    c.setRating = setRating;
+  };
+
+  c.gotoNext = function (step, icon) {
+    c.step = step;
+    c.selectedIcon = icon;
+  }
+
+  c.closeSurvery = function () {
+    c.showSurvey = false;
+  }
+
+  function setRating(event) {
+    angular.element(event.target).addClass('selected animated rubberBand');
+    $timeout(function () {
+      angular.element('.emoji-rating').addClass('animated fadeOut');
+    }, 1000);
+  }
 }
