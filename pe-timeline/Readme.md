60537cb29d91bf14644a0b216e8f8257e30083a7
fabio.petito <fabio.petito@servicenow.com>
2017-03-01 01:30:08 -0800

    Timeline

--- pe-timeline/Readme.md
+++ pe-timeline/Readme.md
@@ -1,8 +1,21 @@
 ## Synopsis
 
-This widget can be used to create a simple signature pad/scratch pad.
+This widget can be used to quickly opbtain an initial implementation of a timeline.
 
 
 ## Installation
 
-Installation is very simple, you can just download the update set "pe-scratch-pad-update-set.xml" and install it on your instance. Then the widget is available for you to drag and drop on your page.
+Installation is very simple, you can just download the update set "pe-timeline-update-set.xml" and install it on your instance. Then the widget is available for you to drag and drop on your page.
+
+We provide few options to load data easily and also to change the appearance of the widget.
+
+**"Title"** This is for changing the title in the header
+
+***
+
+We provide four SASS variables to control the color of the statistics shown in the timeline (both text and icon):
+
+`$pe-timeline-items-color: #ff6f00 !default;`
+
+You can overide these variables at portal level using the **themes**.
+
--- pe-timeline/widget_client/Timeline.js
+++ pe-timeline/widget_client/Timeline.js
@@ -1,42 +1,61 @@
-function($rootScope, $scope) {
+function ($rootScope, $scope) {
   /* widget controller */
   var c = this;
 
   Object.size = function (obj) {
-    var size = 0, key;
+    var size = 0,
+      key;
     for (key in obj) {
       if (obj.hasOwnProperty(key)) size++;
     }
     return size;
   };
 
-  c.initialEvents = [
-    {
+  c.initialEvents = [{
       description: 'Automated Event Correlation',
       time: '14:22',
       icon: 'fa fa-bolt fa-lg',
-      stats: [
-        { name: 'Events', value: 20 },
-        { name: 'Sources', value: 5 }
+      stats: [{
+          name: 'Events',
+          value: 20
+        },
+        {
+          name: 'Sources',
+          value: 5
+        }
       ]
     },
     {
       description: 'Automated Service Correlation',
       time: '14:22',
       icon: 'fa fa-bolt fa-lg',
-      stats: [
-        { name: 'In Total', value: 2 },
-        { name: 'Tier 1 Service', value: 1, iconSuffix: 'fa fa-shopping-cart fa-lg' }
+      stats: [{
+          name: 'In Total',
+          value: 2
+        },
+        {
+          name: 'Tier 1 Service',
+          value: 1,
+          iconSuffix: 'fa fa-shopping-cart fa-lg'
+        }
       ]
     },
     {
       description: 'Automated Risk Assessment',
       time: '14:23',
       icon: 'fa fa-bolt fa-lg',
-      stats: [
-        { value: '1.2M', icon: 'fa fa-usd fa-lg' },
-        { value: '42K', icon: 'fa fa-users fa-lg' },
-        { value: '3', icon: 'fa fa-globe fa-lg' }
+      stats: [{
+          value: '1.2M',
+          icon: 'fa fa-usd fa-lg'
+        },
+        {
+          value: '42K',
+          icon: 'fa fa-users fa-lg'
+        },
+        {
+          value: '3',
+          icon: 'fa fa-globe fa-lg'
+        }
       ]
     }
   ];
