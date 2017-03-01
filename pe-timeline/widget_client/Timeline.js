bf7bc56902ce51c5f84d30bc7ddf0cfc3267b3d4
fabio.petito <fabio.petito@servicenow.com>
2017-02-28 18:32:08 -0800

    Timeline

--- pe-timeline/widget_client/Timeline.js
+++ pe-timeline/widget_client/Timeline.js
@@ -2,125 +2,78 @@ function($rootScope, $scope) {
   /* widget controller */
   var c = this;
 
+  Object.size = function (obj) {
+    var size = 0, key;
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+  };
+
   c.initialEvents = [
-  	{ description: "Automated Event Correlation",
-      time: "14:22",
-      icon: "#flash",
+    {
+      description: 'Automated Event Correlation',
+      time: '14:22',
+      icon: 'fa fa-bolt fa-lg',
       stats: [
-        { name: "Events", value: 20 },
-        { name: "Sources", value: 5 }
+        { name: 'Events', value: 20 },
+        { name: 'Sources', value: 5 }
       ]
     },
-  	{ description: "Automated Service Correlation",
-      time: "14:22",
-      icon: "#flash",
+    {
+      description: 'Automated Service Correlation',
+      time: '14:22',
+      icon: 'fa fa-bolt fa-lg',
       stats: [
-        { name: "In Total", value: 2 },
-        { name: "Tier 1 Service", value: 1, iconSuffix: "#shopping" }
+        { name: 'In Total', value: 2 },
+        { name: 'Tier 1 Service', value: 1, iconSuffix: 'fa fa-shopping-cart fa-lg' }
       ]
     },
-    { description: "Automated Risk Assessment",
-      time: "14:23",
-      icon: "#flash",
+    {
+      description: 'Automated Risk Assessment',
+      time: '14:23',
+      icon: 'fa fa-bolt fa-lg',
       stats: [
-        { value: '1.2M', icon: "#dollar" },
-        { value: '42K', icon: "#people" },
-        { value: '3', icon: '#world' }
-      ]
-    },
-    { description: "Service Owners Notified",
-      time: "14:26",
-      icon: "#flash",
-      users: [
-        { photo: 'user1.jpg' },
-        { photo: 'user2.jpg' },
-        { photo: 'user3.jpg' }
-      ]
-    },
-    { description: "Correlate Problems and Change History",
-      time: "14:29",
-      icon: "#flash",
-      stats: [
-        { name: "Changes", value: 3 },
-        { name: "Problem", value: 1 }
-      ]
-    },
-    { description: "Generate Remediate Actions",
-      time: "14:32",
-      icon: "#flash",
-      stats: [
-        { name: "Possible Actions", value: 3 }
+        { value: '1.2M', icon: 'fa fa-usd fa-lg' },
+        { value: '42K', icon: 'fa fa-users fa-lg' },
+        { value: '3', icon: 'fa fa-globe fa-lg' }
       ]
     }
   ];
 
-  c.remediationEvents = [
-    { description: "CHG000693 Created", time: "14:35" },
-    { description: "Temp Log File Size Increased", time: "14:37" },
-    { description: "SQL2012 Hotfix Uninstalled", time: "14:41" },
-    { description: "SQL Cluster Restarted – Active", time: "14:45" }
-  ];
+  c.title = 'Timeline'
+  c.itemCount = Object.size(c.initialEvents);
+  c.fadeOldEvents = true;
+  c.shownInitialEvents = 0;
 
-  c.newEvents = [
-    { description: "Synthetic User Testing Completed",
-      time: "14:45",
-      icon: "#check-mark"
-    },
-    { description: "CHG000693 Closed",
-      time: "14:46",
-      icon: "#flash"
-    },
-    { description: "Business Owners Notified of Prevention",
-      time: "14:48",
-      users: [
-        { photo: 'user1.jpg' },
-        { photo: 'user2.jpg' },
-        { photo: 'user3.jpg' }
-      ],
-      icon: "#flash"
-    },
-    { description: "Machine Learning Results Updated",
-      time: "14:51",
-      icon: "#flash"
-    }
-  ];
 
-  $rootScope.$broadcast('countItems', { widget: 'outprev-remediation-timeline', count: c.initialEvents.length });
-
-  $rootScope.$on('recovered', function(){
+  $rootScope.$on('recovered', function () {
     c.recovered = true;
     c.shownInitialEvents = 0;
     $rootScope.$broadcast('countItems', {
-      widget: 'outprev-remediation-timeline',
+      widget: 'pe-timeline',
       count: 3 + c.newEvents.length
     });
   });
-  c.fadeOldEvents = true;
 
-  c.shownInitialEvents = 0;
-  c.nextInitialEvent = function(){
-  	if (c.shownInitialEvents < c.initialEvents.length) {
-  		c.shownInitialEvents++
-  	} else {
-  		c.shownInitialEvents = 0;
+
+  c.nextInitialEvent = function () {
+    if (c.shownInitialEvents < c.initialEvents.length) {
+      c.shownInitialEvents++
+    } else {
+      c.shownInitialEvents = 0;
       c.fadeOldEvents = true;
-  	}
+    }
   }
-  c.expandInitialEvents = function(){
+
+  c.expandInitialEvents = function () {
     c.shownInitialEvents = c.initialEvents.length;
     c.fadeOldEvents = false;
   }
-  c.collapseInitialEvents = function(){
+
+  c.collapseInitialEvents = function () {
     c.shownInitialEvents = 0;
     c.fadeOldEvents = true;
-  }
-
-  c.shownRemediationEvents = 0;
-  c.expandRemediationEvents = function(){
-    c.shownRemediationEvents = c.remediationEvents.length;
-  }
-  c.collapseRemediationEvents = function(){
-    c.shownRemediationEvents = 0;
   }
 
 }
