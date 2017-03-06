e0b3014f9993a256354571255349a8c1bfca612e
Steven Gregory <steven.gregory@servicenow.com>
2017-03-06 16:08:16 -0600

    Refactored widget timeline

--- pe-timeline/widget_client/PE Timeline.js
+++ pe-timeline/widget_client/PE Timeline.js
@@ -1,93 +1,91 @@
-function ($rootScope, $scope) {
-  /* widget controller */
+function TimelineController() {
   var c = this;
+  c.nextInitialEvent = nextInitialEvent;
+  c.expandInitialEvents = expandInitialEvents;
+  c.collapseInitialEvents = collapseInitialEvents;
 
-  Object.size = function (obj) {
-    var size = 0,
-      key;
-    for (key in obj) {
-      if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
+  c.$onInit = function() {
+    c.title = c.data.title;
+    c.initialEvents = getInitialEvents();
+    c.itemsCount = c.initialEvents.length;
+    c.fadeOldEvents = false;
   };
 
-  c.initialEvents = [{
-    description: 'Automated Event Correlation',
-    leftDescription: '14:22',
-    icon: 'fa fa-bolt fa-lg',
-    color: '#dbcb33',
-    stats: [{
-      name: 'Events',
-      value: 20
-    },
-    {
-      name: 'Sources',
-      value: 5
+  function getInitialEvents() {
+    c.shownInitialEvents = c.data.initial_elements;
+    if (c.shownInitialEvents === undefined) {
+      c.shownInitialEvents = 0;
     }
-    ]
-  },
-  {
-    description: 'Automated Service Correlation',
-    leftDescription: '14:22',
-    icon: 'fa fa-bolt fa-lg',
-    color: '#d86431',
-    stats: [{
-      name: 'In Total',
-      value: 2
-    },
-    {
-      name: 'Tier 1 Service',
-      value: 1,
-      iconSuffix: 'fa fa-shopping-cart fa-lg'
-    }
-    ]
-  },
-  {
-    description: 'Automated Risk Assessment',
-    leftDescription: '14:23',
-    icon: 'fa fa-bolt fa-lg',
-    color: '#903f5c',
-    stats: [{
-      value: '1.2M',
-      icon: 'fa fa-usd fa-lg'
-    },
-    {
-      value: '42K',
-      icon: 'fa fa-users fa-lg'
-    },
-    {
-      value: '3',
-      icon: 'fa fa-globe fa-lg'
-    }
-    ]
+    var events = [{
+        description: 'Automated Event Correlation',
+        leftDescription: '14:22',
+        icon: 'fa fa-bolt fa-lg',
+        color: '#dbcb33',
+        stats: [{
+            name: 'Events',
+            value: 20
+          },
+          {
+            name: 'Sources',
+            value: 5
+          }
+        ]
+      },
+      {
+        description: 'Automated Service Correlation',
+        leftDescription: '14:22',
+        icon: 'fa fa-bolt fa-lg',
+        color: '#d86431',
+        stats: [{
+            name: 'In Total',
+            value: 2
+          },
+          {
+            name: 'Tier 1 Service',
+            value: 1,
+            iconSuffix: 'fa fa-shopping-cart fa-lg'
+          }
+        ]
+      },
+      {
+        description: 'Automated Risk Assessment',
+        leftDescription: '14:23',
+        icon: 'fa fa-bolt fa-lg',
+        color: '#903f5c',
+        stats: [{
+            value: '1.2M',
+            icon: 'fa fa-usd fa-lg'
+          },
+          {
+            value: '42K',
+            icon: 'fa fa-users fa-lg'
+          },
+          {
+            value: '3',
+            icon: 'fa fa-globe fa-lg'
+          }
+        ]
+      }
+    ];
+    return events;
   }
-  ];
 
-  c.title = c.data.title;
-  c.itemsCount = Object.size(c.initialEvents);
-  c.fadeOldEvents = false;
-  c.shownInitialEvents = c.data.initial_elements;
-  if (c.shownInitialEvents === undefined) {
-    c.shownInitialEvents = 0;
-  }
-
-  c.nextInitialEvent = function () {
+  function nextInitialEvent() {
     if (c.shownInitialEvents < c.initialEvents.length) {
-      c.shownInitialEvents++
+      c.shownInitialEvents++;
     } else {
       c.shownInitialEvents = 0;
       c.fadeOldEvents = false;
     }
   }
 
-  c.expandInitialEvents = function () {
+  function expandInitialEvents() {
     c.shownInitialEvents = c.initialEvents.length;
     c.fadeOldEvents = false;
   }
 
-  c.collapseInitialEvents = function () {
+  function collapseInitialEvents() {
     c.shownInitialEvents = 0;
     c.fadeOldEvents = false;
   }
-
 }
