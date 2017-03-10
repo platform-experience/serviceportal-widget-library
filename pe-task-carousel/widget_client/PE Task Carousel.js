7b1163cfbb1b2e6a80cfaa4cc70783f143ef2a1f
stevengregory <steven.gregory@servicenow.com>
2017-03-10 10:54:18 -0600

    Added charts to carousel widget

--- pe-task-carousel/widget_client/PE Task Carousel.js
+++ pe-task-carousel/widget_client/PE Task Carousel.js
@@ -1,4 +1,4 @@
-function TaskCarouselController() {
+function TaskCarouselController(spUtil) {
   var c = this;
   c.prevPhase = prevPhase;
   c.nextPhase = nextPhase;
@@ -6,8 +6,27 @@ function TaskCarouselController() {
   c.toggleTasks = toggleTasks;
 
   c.$onInit = function() {
+    activateCharts();
     setDefaults();
   };
+
+  function activateCharts() {
+    var count = 0;
+    c.sparkline = [];
+    while (count !== 18) {
+      if (!c.sparkline[count]) {
+        spUtil.get('pe-sparkline', {
+          options: {
+            gradientEnd: 'rgba(0, 59, 210, 1)',
+            gradientStart: 'rgba(0, 201, 199, 1)'
+          }
+        }).then(function(response) {
+          c.sparkline.push(response);
+        });
+      }
+      count++;
+    }
+  }
 
   function prevPhase() {
     angular.element('.carousel').carousel('prev');
