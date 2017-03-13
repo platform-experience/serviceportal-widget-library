814026c639832d1a98824ad05548841cb9d6ad02
fabio.petito <fabio.petito@servicenow.com>
2017-03-13 14:09:16 -0700

    pe-horizontal-stacked-bar

--- pe-horizontal-stacked-bar/widget_server/PE Horizontal Stacked Bar.js
+++ pe-horizontal-stacked-bar/widget_server/PE Horizontal Stacked Bar.js
@@ -3,8 +3,6 @@
   /* e.g., data.table = $sp.getValue('table'); */
   data.lnumber = '$230';
   data.lnumber_text = 'SPENT';
-
   data.graph_data = input.graph_data || options.graph_data;
   data.graph_data = JSON.parse(data.graph_data);
-
 })();
