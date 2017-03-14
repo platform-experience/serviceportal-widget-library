ef5dc7eadde2224ace2eebac601647ec9c924aec
fabio.petito <fabio.petito@servicenow.com>
2017-03-14 12:05:40 -0700

    widget commit

---
+++ Overview Cards/pe-overview-card-snippet/widget_server/PE Overview Card Snippet.js
@@ -0,0 +1,7 @@
+(function () {
+  /* populate the 'data' object */
+  /* e.g., data.table = $sp.getValue('table'); */
+  data.text = 'Starts in ';
+  data.days = '2 days';
+  data.user_sys_id = gs.getUserID();
+})();
