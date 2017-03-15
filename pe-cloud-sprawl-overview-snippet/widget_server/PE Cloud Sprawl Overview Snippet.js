5c5c5694a1f7ffee6b9aa71e566f8c35d4940809
fabio.petito <fabio.petito@servicenow.com>
2017-03-15 15:41:58 -0700

    widget commit

--- pe-cloud-sprawl-overview-snippet/widget_server/PE Cloud Sprawl Overview Snippet.js
+++ pe-cloud-sprawl-overview-snippet/widget_server/PE Cloud Sprawl Overview Snippet.js
@@ -2,8 +2,6 @@
   /* populate the 'data' object */
   /* e.g., data.table = $sp.getValue('table'); */
   data.card_data = input.card_data || options.card_data;
-  data.card_data = JSON.parse(data.card_data);
-
   if (data.isJson(data.card_data) === false) {
     data.card_data = JSON.parse(data.card_data);
   };
