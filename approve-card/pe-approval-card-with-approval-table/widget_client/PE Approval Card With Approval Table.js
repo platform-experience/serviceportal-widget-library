6e58ea87190837299c0314f2ac4b6db63155c915
Arthur Oliveira <arthur.gmraes@gmail.com>
2017-03-23 12:42:38 -0700

    including approval card from approval card  with table

--- approve-card/pe-approval-card-with-approval-table/widget_client/PE Approval Card With Approval Table.js
+++ approve-card/pe-approval-card-with-approval-table/widget_client/PE Approval Card With Approval Table.js
@@ -1,49 +1,22 @@
-function ApprovalCardController(spUtil) {
+function ApprovalCardController(spUtil, $scope) {
   var c = this;
 
-  c.approve = approve;
-
-  c.reject = reject;
-
-  c.view = view;
-
-  c.data.widget = {
-    title: c.data.task.short_description.display_value || c.options.title,
-    purpose: c.options.purpose || c.data.task.request_item.display_value,
-    icon: c.options.icon
-  };
-
-  c.data.user_info = {
-    user_sys_id: c.data.task.assigned_to.value,
-    show_only_picture: false,
-    show_job_title: true,
-    show_call_and_chat: false
-  };
-
-  spUtil.get('pe-people-info', {
-    user_sys_id: c.data.user_info.user_sys_id,
-    show_job_title: c.data.user_info.show_job_title,
-    show_call_and_chat: c.data.user_info.show_call_and_chat,
-    show_only_picture: c.data.user_info.show_only_picture
+  spUtil.get('pe-approval-card', {
   }).then(function (response) {
     c.data.embedded_widget = response;
   });
 
-  function approve() {
+  $scope.$on('cart-solved', function (event, param) {
     c.server.get({action: 'approve'}).then(function () {
-      $rootScope.$broadcast('cart-solved', {card: c, status: 'approved'});
-      c.submitted = true;
     })
-  }
+  });
 
-  function reject() {
-    c.server.get({action: 'reject'}).then(function () {
-      $rootScope.$broadcast('cart-solved', {card: c, status: 'rejected'});
-      c.submitted = true;
+  $scope.$on('cart-reject', function (event, param) {
+    c.server.get({action: 'approve'}).then(function () {
     })
-  }
+  });
 
-  function view() {
-    $rootScope.$broadcast('cart-view', {table: "x", record: 'y'});
-  }
+  $scope.$on('cart-view', function (event, param) {
+    // {table: "x", record: 'y'}
+  });
 }
