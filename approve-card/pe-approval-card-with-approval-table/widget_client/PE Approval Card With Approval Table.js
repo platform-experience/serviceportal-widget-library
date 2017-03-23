35f4a643836a0434dca46d59d4e075da9e2b06ae
Arthur Oliveira <arthur.gmraes@gmail.com>
2017-03-23 15:43:34 -0700

    Approval table passing data as parameter to approval card widget

--- approve-card/pe-approval-card-with-approval-table/widget_client/PE Approval Card With Approval Table.js
+++ approve-card/pe-approval-card-with-approval-table/widget_client/PE Approval Card With Approval Table.js
@@ -1,7 +1,15 @@
 function ApprovalCardController(spUtil, $scope) {
   var c = this;
 
-  spUtil.get('pe-approval-card', {}).then(function (response) {
+  var widget_param = {
+    title: c.data.task.short_description.display_value || c.options.title,
+    purpose: c.data.task.request_item.display_value || c.options.purpose,
+    icon: c.options.icon,
+    user: c.data.task.assigned_to.value
+  };
+
+
+  spUtil.get('pe-approval-card', widget_param).then(function (response) {
     c.data.embedded_widget = response;
   });
 
