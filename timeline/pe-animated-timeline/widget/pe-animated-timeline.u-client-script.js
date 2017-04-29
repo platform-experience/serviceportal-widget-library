e2dbcfd4f0c983fbf90c2f6e89e6609e73644a71
stevengregory <sgregory@gmail.com>
2017-04-28 22:00:15 -0500

    Refactored animated timeline widget

--- timeline/pe-animated-timeline/widget/pe-animated-timeline.u-client-script.js
+++ timeline/pe-animated-timeline/widget/pe-animated-timeline.u-client-script.js
@@ -1,31 +1,21 @@
 function AnimatedTimelineController($timeout) {
   var c = this;
 
-  if (c.data.interval) {
-    $timeout.cancel(c.data.interval);
-    c.data.items.forEach(function(item) {
-      item.completed = false;
-    });
-    c.data.completed = false;
-  }
-
-  activateNext(c.data.items);
+  c.$onInit = function() {
+    activateNext(c.data.items);
+  };
 
   function activateNext(items) {
-    if (typeof(c.itemActivated) === 'undefined')
+    if (typeof(c.itemActivated) === 'undefined') {
       c.itemActivated = -1;
+    }
     c.itemActivated++;
-
-    c.data.interval = $timeout(function() {
+    $timeout(function() {
       items[c.itemActivated].completed = true;
       if (c.itemActivated < items.length) {
         if (!items[c.itemActivated].end) {
           activateNext(items);
-        } else {
-          group.end = true;
         }
-      } else {
-        group.end = true;
       }
     }, items[c.itemActivated].delay * 1000);
   }
--- timeline/pe-animated-timeline/widget/pe-animated-timeline.u-server-script.js
+++ timeline/pe-animated-timeline/widget/pe-animated-timeline.u-server-script.js
@@ -1,18 +1,18 @@
 (function() {
-  var gr = GlideRecord('u_pe_animated_timeline');
-  gr.orderBy('u_order');
-  gr.query();
+  var grTimeline = GlideRecord('u_pe_animated_timeline');
+  grTimeline.orderBy('u_order');
+  grTimeline.query();
   var items = [];
   var obj;
-  while (gr.next()) {
+  while (grTimeline.next()) {
     obj = {};
-    obj.name = gr.u_name.toString();
-    obj.subtitle = gr.u_subtitle.toString();
-    obj.html = gr.u_html_description.toString();
-    obj.delay = parseInt(gr.u_delay.toString());
-    obj.fail = gr.u_fail.toString() === 'true';
-    obj.end = gr.u_end.toString() === 'true';
-    obj.order = parseInt(gr.u_order.toString());
+    obj.name = grTimeline.u_name.toString();
+    obj.subtitle = grTimeline.u_subtitle.toString();
+    obj.html = grTimeline.u_html_description.toString();
+    obj.delay = parseInt(grTimeline.u_delay.toString());
+    obj.fail = grTimeline.u_fail.toString() === 'true';
+    obj.end = grTimeline.u_end.toString() === 'true';
+    obj.order = parseInt(grTimeline.u_order.toString());
     items.push(obj);
   }
   data.items = items;
