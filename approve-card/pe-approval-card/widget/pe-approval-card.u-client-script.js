308bf633293b6eb57f883493edd8ab22d1ea7cdc
stevengregory <sgregory@gmail.com>
2017-06-01 15:42:50 -0500

    Refactoring & fixes on approval card widget

--- approve-card/pe-approval-card/widget/pe-approval-card.u-client-script.js
+++ approve-card/pe-approval-card/widget/pe-approval-card.u-client-script.js
@@ -1,25 +1,37 @@
 function ApprovalCardController(spUtil) {
-    var c = this;
+  var c = this;
 
-    c.data.widget = {
-        title: c.options.title,
-        purpose: c.options.purpose,
-        icon: c.options.icon
-    };
+  c.$onInit = function() {
+    getUserInfo();
+    getWidgetOptions();
+    activateWidget();
+  };
 
-    c.data.user_info = {
-        user_sys_id: '9ec35b8713453a007e94fc5ed144b09a',
-        show_only_picture: false,
-        show_job_title: true,
-        show_call_and_chat: false
-    };
-    /*Inject People info widget here*/
+  function activateWidget() {
     spUtil.get('pe-people-info', {
-        user_sys_id: c.data.user_info.user_sys_id,
-        show_job_title: c.data.user_info.show_job_title,
-        show_call_and_chat: c.data.user_info.show_call_and_chat,
-        show_only_picture: c.data.user_info.show_only_picture
+      userSysId: c.data.userInfo.user_sys_id,
+      showOnlyPicture: c.data.userInfo.show_job_title,
+      showJobTitle: c.data.userInfo.show_call_and_chat,
+      showCallAndChat: c.data.userInfo.show_only_picture
     }).then(function(response) {
-        c.data.embedded_widget = response;
+      c.data.peopleInfoWidget = response;
     });
+  }
+
+  function getUserInfo() {
+    c.data.userInfo = {
+      userSysId: '9ec35b8713453a007e94fc5ed144b09a',
+      showOnlyPicture: false,
+      showJobTitle: true,
+      showCallAndChat: false
+    };
+  }
+
+  function getWidgetOptions() {
+    c.data.widget = {
+      title: c.options.title,
+      purpose: c.options.purpose,
+      icon: c.options.icon
+    };
+  }
 }
