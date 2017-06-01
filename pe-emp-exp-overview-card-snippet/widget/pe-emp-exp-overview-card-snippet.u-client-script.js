9db2e4aa87c5de527f522f0633a3099b75dc4e95
stevengregory <sgregory@gmail.com>
2017-06-01 18:38:26 -0500

    Overview card widget updates & refactoring

--- pe-emp-exp-overview-card-snippet/widget/pe-emp-exp-overview-card-snippet.u-client-script.js
+++ pe-emp-exp-overview-card-snippet/widget/pe-emp-exp-overview-card-snippet.u-client-script.js
@@ -1,16 +1,19 @@
-function OverviewCardSnippetController(spUtil) {
-  /* widget controller */
+function OverviewCardController(spUtil) {
   var c = this;
 
-  spUtil.get('pe-people-info', {
-    user_sys_id: c.data.user_sys_id,
-    show_job_title: 'false',
-    show_call_and_chat: 'false',
-    show_only_picture: 'false',
-    show_text_below_picture: 'true'
-  }).then(function (response) {
-    c.data.embedded_widget_1 = response;
-  });
+  c.$onInit = function() {
+    activateWidget();
+  };
 
-
+  function activateWidget() {
+    spUtil.get('pe-people-info', {
+      user_sys_id: c.data.user_sys_id,
+      show_job_title: false,
+      show_call_and_chat: false,
+      show_only_picture: false,
+      show_text_below_picture: true
+    }).then(function(response) {
+      c.data.peopleInfoWidget = response;
+    });
+  }
 }
--- pe-emp-exp-overview-card-snippet/widget/pe-emp-exp-overview-card-snippet.u-server-script.js
+++ pe-emp-exp-overview-card-snippet/widget/pe-emp-exp-overview-card-snippet.u-server-script.js
@@ -1,6 +1,4 @@
-(function () {
-  /* populate the 'data' object */
-  /* e.g., data.table = $sp.getValue('table'); */
+(function() {
   data.text = 'Starts in ';
   data.days = '2 days';
   data.user_sys_id = gs.getUserID();
