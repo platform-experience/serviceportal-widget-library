4e7d1c0251747dfd1160bdb22d664d51a771ad01
stevengregory <sgregory@gmail.com>
2017-06-12 18:43:37 -0500

    Updated status alert widget & dependencies

--- pe-status-alert/widget/pe-status-alert.u-client-script.js
+++ pe-status-alert/widget/pe-status-alert.u-client-script.js
@@ -1,25 +1,30 @@
-function StatusAlert($interval, $filter) {
-    /* widget controller */
-    var c = this;
-    var d = new Date();
+function StatusAlertController($filter, $interval) {
+  var c = this;
+  var d = new Date();
 
-    c.alert = c.data.alert;
+  c.$onInit = function() {
     c.probabilityGauge = c.data.probabilityGauge;
+    getAlertDate();
+    getOutageEta();
+  };
 
+  function getAlertDate() {
+    c.alert = c.data.alert;
     if (c.alert.created_on) {
-        c.alert.created_on_date = new Date(c.alert.created_on);
-        c.alert.created_on_string = $filter('date')(c.alert.created_on_date, 'short');
+      c.alert.created_on_date = new Date(c.alert.created_on);
+      c.alert.created_on_string = $filter('date')(c.alert.created_on_date, 'short');
     }
+  }
 
-    var outage_eta = new Date(d.getTime() + c.options.outage_eta * 60000);
-    c.min_to_eta = Math.floor((outage_eta.getTime() - d.getTime()) / 60000);
-
+  function getOutageEta() {
+    var outageEta = new Date(d.getTime() + c.options.outage_eta * 60000);
+    c.min_to_eta = Math.floor((outageEta.getTime() - d.getTime()) / 60000);
     if (c.min_to_eta < 0) {
-        c.min_to_eta = 0;
+      c.min_to_eta = 0;
     } else {
-        $interval(function() {
-            c.min_to_eta -= 1;
-        }, 60000, c.min_to_eta);
+      $interval(function() {
+        c.min_to_eta -= 1;
+      }, 60000, c.min_to_eta);
     }
-
+  }
 }
--- pe-status-alert/widget/pe-status-alert.u-server-script.js
+++ pe-status-alert/widget/pe-status-alert.u-server-script.js
@@ -1,98 +1,89 @@
 (function() {
-    /* populate the 'data' object */
-    /* e.g., data.table = $sp.getValue('table'); */
+  var serverOptions = input.options ? input.options : (input.parameters ? input.parameters : {});
+  options.alert_sysid = options.alert_sysid || serverOptions.alert_sysid;
+  options.probability = options.probability || serverOptions.probability || 67;
+  options.outage_eta = options.outage_eta || serverOptions.outage_eta || 30;
 
-    var serverOptions = input.options ? input.options : (input.parameters ? input.parameters : {});
-    options.alert_sysid = options.alert_sysid || serverOptions.alert_sysid;
-    options.probability = options.probability || serverOptions.probability || 67;
-    options.outage_eta = options.outage_eta || serverOptions.outage_eta || 30;
-
-    var getAlert = function(rec) {
-        return {
-            sys_id: rec.sys_id.toString(),
-            number: rec.number.toString(),
-            created_on: rec.sys_created_on.toString(),
-            description: rec.description.toString(),
-            state: rec.state.toString()
-        };
+  var getAlert = function(rec) {
+    return {
+      sys_id: rec.sys_id.toString(),
+      number: rec.number.toString(),
+      created_on: rec.sys_created_on.toString(),
+      description: rec.description.toString(),
+      state: rec.state.toString()
     };
+  };
 
-    var alertGR, alert;
-    if (options.alert_sysid) {
-        alertGR = new GlideRecord('em_alert_anomaly');
-        if (alertGR.get(options.alert)) {
-            alert = getAlert(alertGR);
-        }
-    } else {
-        alertGR = new GlideRecord('em_alert_anomaly');
-        //alertGR.addEncodedQuery('state!=Closed');
-        alertGR.orderByDesc('sys_created_on');
-        alertGR.query();
-        if (alertGR.next()) {
-            alert = getAlert(alertGR);
-        }
+  var alertGR, alert;
+  if (options.alert_sysid) {
+    alertGR = new GlideRecord('em_alert_anomaly');
+    if (alertGR.get(options.alert)) {
+      alert = getAlert(alertGR);
     }
-    data.alert = alert;
+  } else {
+    alertGR = new GlideRecord('em_alert_anomaly');
+    alertGR.orderByDesc('sys_created_on');
+    alertGR.query();
+    if (alertGR.next()) {
+      alert = getAlert(alertGR);
+    }
+  }
+  data.alert = alert;
 
-    data.probabilityGauge = $sp.getWidget("pe-solid-gauge", {
-
-        script_include: 'PEChartData',
-        function_name: 'getData',
-        param1: options.probability,
-
-        advance: JSON.stringify({
-            options: {
-                title: {
-                    text: null
-                },
-                chart: {
-                    height: 140,
-                    width: 140,
-                    backgroundColor: null
-                },
-                pane: {
-                    background: {
-                        backgroundColor: 'rgba(255,255,255,0.5)',
-                        borderColor: null,
-                        shape: 'solid',
-                        innerRadius: '60%',
-                        outerRadius: '100%'
-                    }
-                },
-                exporting: {
-                    enabled: false
-                },
-                legend: {
-                    enabled: false
-                },
-                yAxis: {
-                    min: 0,
-                    max: 100,
-                    tickAmount: 0,
-                    tickWidth: 0,
-                    stops: [
-                        [0, 'rgba(255,255,255,0.5)'],
-                        [1, 'rgba(255,255,255,1)']
-                    ]
-                },
-                plotOptions: {
-                    solidgauge: {
-                        dataLabels: {
-                            format: "{y}%",
-                            color: "white",
-                            style: {
-                                fontFamily: "'SourceSansPro', Helvetica, Arial, sans-serif",
-                                fontSize: "24px",
-                                fontWeight: "400"
-                            },
-                            y: -20
-                        }
-                    }
-                }
+  data.probabilityGauge = $sp.getWidget('pe-solid-gauge', {
+    script_include: 'PEChartData',
+    function_name: 'getData',
+    param1: options.probability,
+    advance: JSON.stringify({
+      options: {
+        title: {
+          text: null
+        },
+        chart: {
+          height: 140,
+          width: 140,
+          backgroundColor: null
+        },
+        pane: {
+          background: {
+            backgroundColor: 'rgba(255,255,255,0.5)',
+            borderColor: null,
+            shape: 'solid',
+            innerRadius: '60%',
+            outerRadius: '100%'
+          }
+        },
+        exporting: {
+          enabled: false
+        },
+        legend: {
+          enabled: false
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          tickAmount: 0,
+          tickWidth: 0,
+          stops: [
+            [0, 'rgba(255,255,255,0.5)'],
+            [1, 'rgba(255,255,255,1)']
+          ]
+        },
+        plotOptions: {
+          solidgauge: {
+            dataLabels: {
+              format: '{y}%',
+              color: 'white',
+              style: {
+                fontFamily: '\'SourceSansPro\', Helvetica, Arial, sans-serif',
+                fontSize: '24px',
+                fontWeight: '400'
+              },
+              y: -20
             }
-
-        })
-
-    });
-
+          }
+        }
+      }
+    })
+  });
 })();
