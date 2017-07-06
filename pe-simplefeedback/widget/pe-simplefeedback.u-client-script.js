4575c01e38872a31a80402c671d27f4a902e98c1
Chris <chris.stoll@servicenow.com>
2017-07-06 11:15:43 -0400

    Fixed Modal issue - iOS Safari Mobile
    iOS Safari Mobile has Modal issues

--- pe-simplefeedback/widget/pe-simplefeedback.u-client-script.js
+++ pe-simplefeedback/widget/pe-simplefeedback.u-client-script.js
@@ -22,7 +22,12 @@ function feedbackController($q, $scope, $http, $location, $window, $rootScope, $
         $scope.$on('openFeedback', function(event, args) {
             $('#feedbackModal').modal('show');
         });
-
+        $('#feedbackModal').on('shown.bs.modal', function() {
+            //Make sure the modal and backdrop are siblings (changes the DOM)
+            $(this).before($('.modal-backdrop'));
+            //Make sure the z-index is higher than the backdrop
+            $(this).css("z-index", parseInt($('.modal-backdrop').css('z-index')) + 1);
+        });
         $('#feedbackModal').on('hidden.bs.modal', function() {
             if ($scope.feedbackSubmitted) {
                 $timeout(function() {
