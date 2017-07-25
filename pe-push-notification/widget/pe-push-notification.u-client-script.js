8ecf08523c0619dbae594e679b6a5a8309008e80
fabio.petito <fabio.petito@servicenow.com>
2017-07-25 11:17:18 +0100

    bug fixing and test new locale management

--- pe-push-notification/widget/pe-push-notification.u-client-script.js
+++ pe-push-notification/widget/pe-push-notification.u-client-script.js
@@ -4,9 +4,6 @@ function PushNotificationsController($window, $scope) {
   var d1 = new Date();
   var d = new Date();
 
-
-
-
   var isMobile = {
     Android: function () {
       return navigator.userAgent.match(/Android/i);
@@ -22,32 +19,37 @@ function PushNotificationsController($window, $scope) {
   c.show = false;
   if ($scope.$root.portal && (isMobile.iOS() || isMobile.Android()))
     c.show = true;
-  if (!c.data.notification.date) {
-    d1 = new Date();
-    d = new Date();
-    d.setUTCFullYear(d1.getFullYear());
-    d.setUTCMonth(d1.getMonth());
-    d.setUTCDate(d1.getDate());
-    c.data.notification.date = d.toLocaleDateString($window.navigator.language, {
-      weekday: 'long',
-      day: 'numeric',
-      month: 'long'
-    });
-  }
-  if (!c.data.notification.time) {
-    d1 = new Date();
-    d = new Date();
-    d.setUTCHours(d1.getHours());
-    d.setUTCMinutes(d1.getMinutes());
-    c.data.notification.time = d.toLocaleTimeString($window.navigator.language, {
-      hour: '2-digit',
-      minute: '2-digit'
-    });
-  }
-  console.log('c.data.background_image = ' + c.data.notification.background_image);
-  if (c.data.notification.background_image) {
-    if (c.data.notification.background_image.length > 0)
-      document.getElementById('main-container').style.backgroundImage = "url(" + c.data.notification.background_image + ")";
+
+  if (c.show) {
+
+    if (!c.data.notification.date) {
+      d1 = new Date();
+      d = new Date();
+      d.setUTCFullYear(d1.getFullYear());
+      d.setUTCMonth(d1.getMonth());
+      d.setUTCDate(d1.getDate());
+      c.data.notification.date = d.toLocaleDateString($window.navigator.language, {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+      });
+    }
+
+    if (!c.data.notification.time) {
+      d1 = new Date();
+      d = new Date();
+      d.setUTCHours(d1.getHours());
+      d.setUTCMinutes(d1.getMinutes());
+      c.data.notification.time = d.toLocaleTimeString($window.navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+    }
+
+    if (c.data.notification.background_image) {
+      if (c.data.notification.background_image.length > 0)
+        document.getElementById('main-container').style.backgroundImage = "url(" + c.data.notification.background_image + ")";
+    }
   }
 
   c.goToPage = function () {
--- pe-push-notification/widget/pe-push-notification.u-server-script.js
+++ pe-push-notification/widget/pe-push-notification.u-server-script.js
@@ -2,6 +2,7 @@
   /* populate the 'data' object */
   /* e.g., data.table = $sp.getValue('table'); */
   data.notification = {};
+
   data.notification.page = input.page || options.page;
   data.notification.time = input.home_time || options.home_time;
   data.notification.date = input.home_date || options.home_date;
@@ -11,6 +12,7 @@
   data.notification.body.title = input.body_title || options.body_title;
   data.notification.body.text = input.body_text || options.body_text;
   data.notification.background_image = input.background_image || options.background_image;
+
   data.notification.title = gs.getMessage(data.notification.title);
   data.notification.body.title = gs.getMessage(data.notification.body.title);
   data.notification.body.text = gs.getMessage(data.notification.body.text);
