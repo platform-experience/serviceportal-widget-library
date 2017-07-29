4504aaab8d72f806fc33ad164209ff9628938961
stevengregory <sgregory@gmail.com>
2017-07-28 22:35:10 -0500

    Added time ago days to incident msg

--- pe-incident-message/ui-script/pe-incident-service.u-client-script.js
+++ pe-incident-message/ui-script/pe-incident-service.u-client-script.js
@@ -15,8 +15,10 @@
         var timeDiff = currentTime - problemTime;
         var minDiff = timeDiff / 60000;
         var hourDiff = timeDiff / 3600000;
+        var dayDiff = timeDiff / 86400000;
         timeAgo.minutes = Math.floor(minDiff - 60 * Math.floor(hourDiff));
         timeAgo.hours = Math.floor(Math.abs(hourDiff + 4));
+        timeAgo.days = Math.floor(Math.abs(dayDiff) + 1);
       }
       return timeAgo;
     }
