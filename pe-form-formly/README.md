48b12e3dd21f0ae7f0b53ffdf1b9ea65032a499a
fabio.petito <fabio.petito@servicenow.com>
2017-08-02 17:36:28 +0100

    PE Form Formly

--- pe-form-formly/README.md
+++ pe-form-formly/README.md
@@ -31,127 +31,131 @@ Widget Option Schema parameters:<br/>
 ---
 <br/>
 ```json
-[{
-      'key': 'email',
-      'type': 'input',
-      'templateOptions': {
-        'type': 'email',
-        'label': 'Email address',
-        'placeholder': 'Enter email'
-      }
-    },
-    {
-      'key': 'password',
-      'type': 'input',
-      'templateOptions': {
-        'type': 'password',
-        'label': 'Password',
-        'placeholder': 'Password'
-      }
-    },
-    {
-      'key': 'roles',
-      'type': 'multiCheckbox',
-      'templateOptions': {
-        'label': 'Roles',
-        'options': [{
-          'id': 1,
-          'title': 'Genius'
-        },
-        {
-          'id': 2,
-          'title': 'Administrator'
-        },
-        {
-          'id': 3,
-          'title': 'User'
-        }
-        ],
-        'valueProp': 'id',
-        'labelProp': 'title'
-      }
-    },
-    {
-      'key': 'geniusCheck',
-      'type': 'checkbox',
-      'templateOptions': {
-        'label': 'Genius'
-      }
-    },
-    {
-      'key': 'bioText',
-      'type': 'textarea',
-      'templateOptions': {
-        'label': 'Bio'
-      }
-    },
-    {
-      'key': 'feedback',
-      'type': 'radio',
-      'templateOptions': {
-        'label': 'Do you love ServiceNow?',
-        'options': [{
-          'name': 'I love it!',
-          'value': 'love'
-        },
-        {
-          'name': 'I love it a lot!',
-          'value': 'lovealot'
-        },
-        {
-          'name': 'I can\'t live without',
-          'value': 'extremelove'
-        }
-        ]
-      }
-    },
-    {
-      'key': 'transportation',
-      'type': 'select',
-      'templateOptions': {
-        'label': 'How do you get around in the city',
-        'valueProp': 'name',
-        'options': [{
-          'name': 'Car'
-        },
-        {
-          'name': 'Helicopter'
-        },
-        {
-          'name': 'Sport Utility Vehicle'
-        },
-        {
-          'name': 'Bicycle',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Skateboard',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Walk',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Bus',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Scooter',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Train',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Hot Air Baloon',
-          'group': 'low emissions'
-        }
-        ]
-      }
+[
+  {
+    "key": "email",
+    "type": "input",
+    "templateOptions": {
+      "type": "email",
+      "label": "Email address",
+      "placeholder": "Enter email"
     }
-];
+  },
+  {
+    "key": "password",
+    "type": "input",
+    "templateOptions": {
+      "type": "password",
+      "label": "Password",
+      "placeholder": "Password"
+    }
+  },
+  {
+    "key": "roles",
+    "type": "multiCheckbox",
+    "templateOptions": {
+      "label": "Roles",
+      "options": [
+        {
+          "id": 1,
+          "title": "Genius"
+        },
+        {
+          "id": 2,
+          "title": "Administrator"
+        },
+        {
+          "id": 3,
+          "title": "User"
+        }
+      ],
+      "valueProp": "id",
+      "labelProp": "title"
+    }
+  },
+  {
+    "key": "geniusCheck",
+    "type": "checkbox",
+    "templateOptions": {
+      "label": "Genius"
+    }
+  },
+  {
+    "key": "bioText",
+    "type": "textarea",
+    "templateOptions": {
+      "label": "Bio"
+    }
+  },
+  {
+    "key": "feedback",
+    "type": "radio",
+    "templateOptions": {
+      "label": "Do you love ServiceNow?",
+      "options": [
+        {
+          "name": "I love it!",
+          "value": "love"
+        },
+        {
+          "name": "I love it a lot!",
+          "value": "lovealot"
+        },
+        {
+          "name": "I can't live without",
+          "value": "extremelove"
+        }
+      ]
+    }
+  },
+  {
+    "key": "transportation",
+    "type": "select",
+    "templateOptions": {
+      "label": "How do you get around in the city",
+      "valueProp": "name",
+      "options": [
+        {
+          "name": "Car"
+        },
+        {
+          "name": "Helicopter"
+        },
+        {
+          "name": "Sport Utility Vehicle"
+        },
+        {
+          "name": "Bicycle",
+          "group": "low emissions"
+        },
+        {
+          "name": "Skateboard",
+          "group": "low emissions"
+        },
+        {
+          "name": "Walk",
+          "group": "low emissions"
+        },
+        {
+          "name": "Bus",
+          "group": "low emissions"
+        },
+        {
+          "name": "Scooter",
+          "group": "low emissions"
+        },
+        {
+          "name": "Train",
+          "group": "low emissions"
+        },
+        {
+          "name": "Hot Air Baloon",
+          "group": "low emissions"
+        }
+      ]
+    }
+  }
+]
 ```
 <br/>
 ---
--- pe-form-formly/widget/pe-maps-ngmap.u-server-script.js
+++ pe-form-formly/widget/pe-maps-ngmap.u-server-script.js
@@ -3,126 +3,130 @@
   if (data.form_fields)
     data.form_fields = JSON.parse(data.form_fields);
   else //sample input fields data model
-    data.form_fields = [{
-      'key': 'email',
-      'type': 'input',
-      'templateOptions': {
-        'type': 'email',
-        'label': 'Email address',
-        'placeholder': 'Enter email'
-      }
-    },
-    {
-      'key': 'password',
-      'type': 'input',
-      'templateOptions': {
-        'type': 'password',
-        'label': 'Password',
-        'placeholder': 'Password'
-      }
-    },
-    {
-      'key': 'roles',
-      'type': 'multiCheckbox',
-      'templateOptions': {
-        'label': 'Roles',
-        'options': [{
-          'id': 1,
-          'title': 'Genius'
-        },
-        {
-          'id': 2,
-          'title': 'Administrator'
-        },
-        {
-          'id': 3,
-          'title': 'User'
+    data.form_fields = [
+      {
+        "key": "email",
+        "type": "input",
+        "templateOptions": {
+          "type": "email",
+          "label": "Email address",
+          "placeholder": "Enter email"
         }
-        ],
-        'valueProp': 'id',
-        'labelProp': 'title'
-      }
-    },
-    {
-      'key': 'geniusCheck',
-      'type': 'checkbox',
-      'templateOptions': {
-        'label': 'Genius'
-      }
-    },
-    {
-      'key': 'bioText',
-      'type': 'textarea',
-      'templateOptions': {
-        'label': 'Bio'
-      }
-    },
-    {
-      'key': 'feedback',
-      'type': 'radio',
-      'templateOptions': {
-        'label': 'Do you love ServiceNow?',
-        'options': [{
-          'name': 'I love it!',
-          'value': 'love'
-        },
-        {
-          'name': 'I love it a lot!',
-          'value': 'lovealot'
-        },
-        {
-          'name': 'I can\'t live without',
-          'value': 'extremelove'
+      },
+      {
+        "key": "password",
+        "type": "input",
+        "templateOptions": {
+          "type": "password",
+          "label": "Password",
+          "placeholder": "Password"
         }
-        ]
-      }
-    },
-    {
-      'key': 'transportation',
-      'type': 'select',
-      'templateOptions': {
-        'label': 'How do you get around in the city',
-        'valueProp': 'name',
-        'options': [{
-          'name': 'Car'
-        },
-        {
-          'name': 'Helicopter'
-        },
-        {
-          'name': 'Sport Utility Vehicle'
-        },
-        {
-          'name': 'Bicycle',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Skateboard',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Walk',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Bus',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Scooter',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Train',
-          'group': 'low emissions'
-        },
-        {
-          'name': 'Hot Air Baloon',
-          'group': 'low emissions'
+      },
+      {
+        "key": "roles",
+        "type": "multiCheckbox",
+        "templateOptions": {
+          "label": "Roles",
+          "options": [
+            {
+              "id": 1,
+              "title": "Genius"
+            },
+            {
+              "id": 2,
+              "title": "Administrator"
+            },
+            {
+              "id": 3,
+              "title": "User"
+            }
+          ],
+          "valueProp": "id",
+          "labelProp": "title"
         }
-        ]
+      },
+      {
+        "key": "geniusCheck",
+        "type": "checkbox",
+        "templateOptions": {
+          "label": "Genius"
+        }
+      },
+      {
+        "key": "bioText",
+        "type": "textarea",
+        "templateOptions": {
+          "label": "Bio"
+        }
+      },
+      {
+        "key": "feedback",
+        "type": "radio",
+        "templateOptions": {
+          "label": "Do you love ServiceNow?",
+          "options": [
+            {
+              "name": "I love it!",
+              "value": "love"
+            },
+            {
+              "name": "I love it a lot!",
+              "value": "lovealot"
+            },
+            {
+              "name": "I can't live without",
+              "value": "extremelove"
+            }
+          ]
+        }
+      },
+      {
+        "key": "transportation",
+        "type": "select",
+        "templateOptions": {
+          "label": "How do you get around in the city",
+          "valueProp": "name",
+          "options": [
+            {
+              "name": "Car"
+            },
+            {
+              "name": "Helicopter"
+            },
+            {
+              "name": "Sport Utility Vehicle"
+            },
+            {
+              "name": "Bicycle",
+              "group": "low emissions"
+            },
+            {
+              "name": "Skateboard",
+              "group": "low emissions"
+            },
+            {
+              "name": "Walk",
+              "group": "low emissions"
+            },
+            {
+              "name": "Bus",
+              "group": "low emissions"
+            },
+            {
+              "name": "Scooter",
+              "group": "low emissions"
+            },
+            {
+              "name": "Train",
+              "group": "low emissions"
+            },
+            {
+              "name": "Hot Air Baloon",
+              "group": "low emissions"
+            }
+          ]
+        }
       }
-    }
     ];
 
 })();
