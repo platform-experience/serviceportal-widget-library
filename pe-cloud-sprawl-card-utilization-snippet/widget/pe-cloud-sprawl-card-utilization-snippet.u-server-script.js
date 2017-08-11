8bb5f0b967d648a996a5b6e63becd788d389aa13
stevengregory <sgregory@gmail.com>
2017-08-11 15:38:07 -0500

    Fixes & updates to CS Card Utilization

--- pe-cloud-sprawl-card-utilization-snippet/widget/pe-cloud-sprawl-card-utilization-snippet.u-server-script.js
+++ pe-cloud-sprawl-card-utilization-snippet/widget/pe-cloud-sprawl-card-utilization-snippet.u-server-script.js
@@ -1,8 +1,4 @@
-(function () {
-  /* populate the 'data' object */
-  /* e.g., data.table = $sp.getValue('table'); */
-
-  /* e.g., how to extract current logged user info sample */
+(function() {
   data.user_sys_id = gs.getUserID();
   var userGR = new GlideRecord('sys_user');
   if (userGR.get(data.user_sys_id)) {
@@ -11,8 +7,7 @@
     data.user_full_name = userGR.getDisplayValue('name');
     data.user_job_title = userGR.getDisplayValue('title');
     data.user_photo = userGR.getDisplayValue('photo');
-    data.user_phone = userGR.phone.toString().replace(/[- )(]/g, '');
+    data.user_phone = userGR.phone.toString().replace(/[\- )(]/g, '');
     data.user_initials = data.user_first_name.charAt(0) + data.user_last_name.charAt(0);
   }
-
 })();
