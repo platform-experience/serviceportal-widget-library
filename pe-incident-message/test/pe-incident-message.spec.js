e36cbdfba00567df3be45e68b97ef15d16c542b5
stevengregory <sgregory@gmail.com>
2017-09-14 19:22:11 -0500

    Added sample unit tests & clean-up

---
+++ pe-incident-message/test/pe-incident-message.spec.js
@@ -0,0 +1,21 @@
+describe('Time ago service', function() {
+  beforeEach(module('pe-incident-message'));
+
+  beforeEach(
+    inject(function(_timeService_) {
+      timeService = _timeService_;
+    })
+  );
+
+  it('service should return 3 properties', function() {
+    var pastTime = '2017-08-07 19:22:32';
+    var timeAgo = timeService.getTimeAgo(pastTime);
+    expect(Object.keys(timeAgo).length).toBe(3);
+  });
+
+  it('new problem should be zero minutes ago', function() {
+    var currentTime = new Date();
+    var timeAgo = timeService.getTimeAgo(currentTime);
+    expect(timeAgo.minutes).toEqual(0);
+  });
+});
