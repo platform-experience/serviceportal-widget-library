a919445a8a73830af2daa7773b79b63cd13bba87
Dylan Lindgren <dylan.lindgren@gmail.com>
2018-02-02 04:44:44 +1100

    Feature/Update PE Image Link (#45)
    * Created pe-image-link widget
    
    * Completed widget documentation
    
    * Corrected folder structure of pe-image-link widget.

---
+++ pe-image-link/widget/pe-image-link-u-link-script.js
@@ -0,0 +1,8 @@
+function peImageLinkLink(scope, elem, attr) {
+	
+	scope.style = {
+		'background-image': 'url(' + scope.options.image_url + ')',
+		'background-position': scope.options.image_pos || 'center'
+	};
+	
+}
