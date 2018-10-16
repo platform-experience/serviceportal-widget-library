2abaf403c681d4763ab42974ec5015bb4997bd12
Chris <christopher.j.stoll@gmail.com>
2018-10-16 11:56:20 -0400

    Created Service Portal floating viewport widget (#85)

---
+++ pe-sp-floating-viewport/widget/pe-sp-floating-viewport.u-server-script.js
@@ -0,0 +1,8 @@
+(function() {
+  /* populate the 'data' object */
+  /* e.g., data.table = $sp.getValue('table'); */
+	
+	options.embedded_page = options.embedded_page || "";
+	options.url_system_parameters = options.url_system_parameters || "";
+	
+})();
