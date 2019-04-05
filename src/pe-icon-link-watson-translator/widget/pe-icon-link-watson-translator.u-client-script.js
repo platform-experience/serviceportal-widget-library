dc6c70e36da85b9d6e69a63207352522ba60aaf5
stevengregory <sgregory@gmail.com>
2019-04-04 20:13:46 -0500

    Widget files added

--- src/pe-icon-link-watson-translator/widget/pe-icon-link-watson-translator.u-client-script.js
+++ src/pe-icon-link-watson-translator/widget/pe-icon-link-watson-translator.u-client-script.js
@@ -1,3 +1,8 @@
-function IconLinkWatsonTranslatorController() {
+function ImageLinkTranslatorController() {
   var c = this;
+
+  c.$onInit = function() {
+    c.title = c.data.title;
+    c.short_description = c.data.short_description;
+  };
 }
--- src/pe-icon-link-watson-translator/widget/pe-icon-link-watson-translator.u-server-script.js
+++ src/pe-icon-link-watson-translator/widget/pe-icon-link-watson-translator.u-server-script.js
@@ -1,4 +1,14 @@
 (function() {
-  /* populate the 'data' object */
-  /* e.g., data.table = $sp.getValue('table'); */
+  var watson = new LanguageService();
+  var source = 'English';
+  data.title =
+    options.language === source ? options.title : watson.translate(options.title, options.language);
+  data.short_description =
+    options.language === source
+      ? options.short_description
+      : watson.translate(options.short_description, options.language);
+
+  var gr = $sp.getInstanceRecord();
+  data.href = $sp.getMenuHREF(gr);
+  data.target = options.target || '';
 })();
