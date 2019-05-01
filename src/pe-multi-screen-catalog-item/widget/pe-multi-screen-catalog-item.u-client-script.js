9ccba1e304a6e83bca11b37a1bf24c75cd57e961
stevengregory <sgregory@gmail.com>
2019-05-01 16:58:45 -0500

    Populated widget files

--- src/pe-multi-screen-catalog-item/widget/pe-multi-screen-catalog-item.u-client-script.js
+++ src/pe-multi-screen-catalog-item/widget/pe-multi-screen-catalog-item.u-client-script.js
@@ -1,3 +1,271 @@
-function MultiScreenCatalogItemController() {
+function ($scope, $location, nowAttachmentHandler, spScUtil, spUtil, spModal, McUtilUpload, McUtilContainers) {
   var c = this;
+  var DEFAULT_RECORD_PRODUCER_REDIRECT_PAGE = 'ticket';
+
+
+  c.isFirstScreen = McUtilContainers.isFirstScreen;
+  c.isLastScreen = McUtilContainers.isLastScreen;
+  c.next = McUtilContainers.next;
+  c.previous = McUtilContainers.previous;
+  c.getCurrentScreenTitle = McUtilContainers.getCurrentScreenTitle;
+  c.getScreenCountAsArray = McUtilContainers.getScreenCountAsArray;
+  c.getCurrentScreen = McUtilContainers.getCurrentScreen;
+  c.goToScreen = McUtilContainers.goToScreen;
+  c.getScreenOfField = McUtilContainers.getScreenOfField;
+
+	$scope.isSelect2 = false;
+
+  c.$onInit = function () {
+    McUtilContainers.init(c.data.sc_cat_item._sections, c.data.sc_cat_item.sys_class_name);
+
+    c.requestedFor = {
+      displayValue: c.data.requestedFor.displayValue,
+      value: c.data.requestedFor.value,
+      name: 'requested_for'
+    }
+
+    c.quantity = c.data.quantity ? c.data.quantity + "" : "1";
+  }
+
+  c.cancel = function() {
+    c.setCheckingOut(false);
+  }
+
+  c.getCurrentTitle = function(sys_class_name) {
+    if(c.getCurrentScreen() == 0) {
+      return c.data.sc_cat_item.name;
+    } else if (c.isLastScreen() && !c.isRecordProducer()) {
+      return c.options.checkoutTitle || 'Order Confirmation';
+    } else {
+      return c.getCurrentScreenTitle(sys_class_name, c.data.sc_cat_item.name);
+    }
+  }
+
+  c.isRecordProducer = function(){
+    return c.data.sc_cat_item.sys_class_name === 'sc_cat_item_producer';
+  }
+
+  c.confirmOrder = function() {
+    c.confirmed = 'asking';
+    spModal.open({
+      title: 'Confirm Order?',
+      message: 'Please confirm your order to proceed.'
+    }).then(function(confirmed) {
+      if (confirmed.label == 'OK') {
+        c.triggerOnSubmit();
+      }
+    });
+  }
+
+  c.triggerOnSubmit = function(){
+    if (validateMandatoryFields()) {
+      $scope.data.sc_cat_item.quantity = c.quantity;
+      $scope.data.sc_cat_item.item_action = "order";
+      if (g_form)
+        return g_form.submit();
+      return false;
+    }
+  }
+
+  c.triggerAddToCart = function () {
+    if (validateMandatoryFields()) {
+      $scope.data.sc_cat_item.item_action = "add_to_cart";
+      $scope.data.sc_cat_item.quantity = c.quantity;
+      if (g_form)
+        return g_form.submit();
+      return false;
+    }
+  }
+
+  c.triggerUpdateCart = function() {
+		$scope.data.sc_cat_item.item_action = "update_cart";
+		$scope.data.sc_cat_item.quantity = c.quantity;
+		if (g_form)
+			return g_form.submit();
+		return false;
+	}
+
+  c.hasImage = function() {
+    return c.data.sc_cat_item.picture != '';
+  }
+
+  c.isCartItem = function() {
+    return c.data.is_cart_item;
+  }
+
+  function validateMandatoryFields() {
+    var fieldNames = g_form.getFieldNames();
+    var mandatoryFieldsWithoutValue = fieldNames.filter(function(field) {
+      return g_form.isMandatory(field) && !hasValue(field, g_form);
+    });
+    if (mandatoryFieldsWithoutValue.length > 0) {
+      notifyUserOfMandFieldsWithoutValue(mandatoryFieldsWithoutValue);
+      c.goToScreen(c.getScreenOfField(mandatoryFieldsWithoutValue[0]));
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function notifyUserOfMandFieldsWithoutValue(fieldNames) {
+    g_form.addErrorMessage('Some fields are incomplete');
+    fieldNames.forEach(function(fieldName) {
+      g_form.showFieldMsg(fieldName, "This field must be completed", 'error');
+    });
+  }
+
+  /**
+   * Check whether a particular field on a form has a value
+   * Copied from /scripts/sn/common/clientScript/glideFormFieldFactory.js
+   * @param  {String}  fieldName     The name of the field to check
+   * @param  {Object}  gFormInstance The GlideForm of the form with the field
+   * @return {Boolean}               Whether the field has a value
+   */
+  function hasValue(fieldName, gFormInstance) {
+
+    var field = gFormInstance.getField(fieldName);
+    var value = gFormInstance.getValue(fieldName);
+
+    switch (field.type) {
+      case 'boolean_confirm':
+        return value === 'true';
+      case 'boolean':
+        return true;
+      case 'currency':
+        var currencyValues = value.split(';');
+        return currencyValues[1] && currencyValues[1].length;
+    }
+
+    if (value === null) {
+      return false;
+    }
+
+    if (typeof value === 'undefined') {
+      return false;
+    }
+
+    var trimmed = String(value).trim();
+    return trimmed.length > 0;
+  }
+
+  /* Setup attachment handler */
+  $scope.attachmentHandler = new nowAttachmentHandler(function appendDone () {
+    $scope.$broadcast("sp.attachments.update", c.data.form._attachmentGUID);
+  }, function appendError () {
+    /* $scope.errorMessages.push(error); */
+    /* spUtil.addErrorMessage(error.msg + error.fileName); */
+  });
+  $scope.$evalAsync(function() {
+    $scope.attachmentHandler.setParams($scope.data._attachmentTable, $scope.data._generatedItemGUID, 1024 * 1024 * $scope.data.maxAttachmentSize);
+  });
+
+  // Initialize the McUtilUpload service
+  McUtilUpload.init($scope.data._attachmentTable, $scope.data._generatedItemGUID);
+
+
+  c.getItemId = function () {
+		return $scope.data.sc_cat_item ? $scope.data.sc_cat_item.sys_id : -1;
+  };
+
+  function getOne() {
+    $scope.server.get({
+			action: 'log_request',
+			itemDetails: {sys_id: $scope.data.sc_cat_item.sys_id,
+										name: $scope.data.sc_cat_item.name,
+										sys_class_name: $scope.data.sc_cat_item.sys_class_name}
+    });
+    if ($scope.data.sc_cat_item.sys_class_name == "sc_cat_item_producer") {
+      postCatalogFormRequest().then(function (response) {
+        var a = response.data.result;
+        handleRedirect(a.table, a.sys_id);
+      });
+    } else {
+      if (c.data.sys_properties.twostep) {
+        // Set additional parameters and add them to orderNow function
+        var additionalParms = {
+          sysparm_requested_for: c.requestedFor.value,
+          special_instructions: c.special_instructions,
+          delivery_address: c.deliverTo
+        };
+        spScUtil.orderNow($scope.data.sc_cat_item.sys_id, $scope.data.sc_cat_item.quantity, getVarData($scope.data.sc_cat_item._fields), $scope.data._generatedItemGUID, additionalParms).then(function (response) {
+          var a = response.data.result;
+          $location.search('id=sc_request&is_new_order=true&table=sc_request&sys_id=' + a.sys_id);
+        });
+      } else {
+        spScUtil.orderNow($scope.data.sc_cat_item.sys_id, $scope.data.sc_cat_item.quantity, getVarData($scope.data.sc_cat_item._fields), $scope.data._generatedItemGUID).then(function (response) {
+          var a = response.data.result;
+          $location.search('id=sc_request&is_new_order=true&table=sc_request&sys_id=' + a.sys_id);
+        });
+      }
+    }
+  }
+
+  function handleRedirect(table, sys_id) {
+    var page = '';
+    if (c.isRecordProducer())
+      page = c.options.record_producer_redirect_page || DEFAULT_RECORD_PRODUCER_REDIRECT_PAGE;
+    $location.search(formatRedirectUrl(page, table, sys_id));
+  }
+
+  function formatRedirectUrl(page, table, sys_id) {
+		var url;
+		var paramObj = {page: page, table: table, sys_id: sys_id};
+		url = spUtil.format(c.options.url, paramObj);
+		return url;
+	}
+
+  function getVarData(fields) {
+		var reqData = {};
+		for(var obj in fields)
+			reqData[fields[obj].name] = fields[obj].value;
+		return reqData;
+	}
+
+  function postCatalogFormRequest() {
+		if ($scope.data.sc_cat_item.sys_class_name === "sc_cat_item_producer")
+			return spScUtil.submitProducer($scope.data.sc_cat_item.sys_id, getVarData($scope.data.sc_cat_item._fields), $scope.data._generatedItemGUID/*, $scope.data.workspaceParams*/);
+    else if ($scope.data.sc_cat_item.item_action === "add_to_cart")
+      return spScUtil.addToCart($scope.data.sc_cat_item.sys_id, $scope.data.sc_cat_item.quantity, getVarData($scope.data.sc_cat_item._fields), $scope.data._generatedItemGUID);
+    else if ($scope.data.is_cart_item)
+			return spScUtil.updateCart($scope.data._generatedItemGUID, $scope.data.sc_cat_item.quantity, getVarData($scope.data.sc_cat_item._fields));
+  }
+
+  function addToCart() {
+    $scope.server.get({
+			action: 'log_request',
+			itemDetails: {sys_id: $scope.data.sc_cat_item.sys_id,
+										name: $scope.data.sc_cat_item.name,
+										sys_class_name: $scope.data.sc_cat_item.sys_class_name}
+    });
+    postCatalogFormRequest().then(function(response) {
+      $rootScope.$broadcast("$sp.service_catalog.cart.add_item");
+      $rootScope.$broadcast("$sp.service_catalog.cart.update");
+      $scope.$emit("$sp.sc_cat_item.add_to_cart", $scope.data._generatedItemGUID);
+      spUtil.addInfoMessage('Your item has been added to your Cart.');
+      // Refresh the server data so that we won't get a duplicate order
+      $scope.server.refresh();
+    });
+  }
+
+  function updateCart() {
+		postCatalogFormRequest().then(function(response) {
+			$location.search('id=sc_cart');
+		})
+	}
+
+	var g_form;
+	$scope.$on('spModel.gForm.initialized', function(e, gFormInstance){
+    if (gFormInstance.getSysId() != -1 && gFormInstance.getSysId() != c.getItemId())
+			return;
+    g_form = gFormInstance;
+    // This runs after all onSubmit scripts have executed
+		g_form.$private.events.on('submitted', function(){
+			if ($scope.data.sc_cat_item.item_action === "order")
+        getOne();
+      else if ($scope.data.sc_cat_item.item_action === "add_to_cart")
+        addToCart();
+      else if ($scope.data.sc_cat_item.item_action == "update_cart")
+				updateCart();
+		});
+	});
 }
--- src/pe-multi-screen-catalog-item/widget/pe-multi-screen-catalog-item.u-server-script.js
+++ src/pe-multi-screen-catalog-item/widget/pe-multi-screen-catalog-item.u-server-script.js
@@ -1,4 +1,198 @@
-(function() {
-  /* populate the 'data' object */
-  /* e.g., data.table = $sp.getValue('table'); */
-})();
+// populate the 'data' variable with catalog item, variables, and variable view
+(function () {
+
+  options.url = options.url || 'id={page}&is_new_order=true&table={table}&sys_id={sys_id}';
+  options.disable_req_for = options.disable_req_for || true;
+  // Comma separated fields to display in snRecordPicker directive
+  options.reqForDispCols = options.reqForDispCols || 'name';
+  options.reqForQuery = options.reqForQuery || 'active=true';
+  // Comma separated fields to search for in snRecordPicker directive
+  options.reqForSearchCols = options.reqForSearchCols || 'name';
+
+  if (input && input.action === 'log_request') {
+    $sp.logStat('Cat Item Request', input.itemDetails.sys_class_name, input.itemDetails.sys_id, input.itemDetails.name);
+    return;
+  }
+
+  // Are we using a two-step checkout process?
+  data.sys_properties = {
+    twostep: gs.getProperty("glide.sc.sp.twostep", "true") == 'true'
+    //twostep: false
+  };
+
+  // Define our initial set of choices and value for the quantity field
+  var clGenerator = new GlideChoiceList();
+  var choiceListQuantity = clGenerator.getChoiceList("sc_cart_item", "quantity");
+  var choicelistQuantityData = [];
+  //Function call changed from size() to getSize() for ScopedChoiceList
+  for (var i = 0; i < choiceListQuantity.getSize(); i++) {
+    //Function call changed from get() to getChoice() for ScopedChoiceList
+    var choice = choiceListQuantity.getChoice(i);
+    if (!isNaN(choice.getValue()))
+      choicelistQuantityData.push({ value: parseInt(choice.getValue()), label: choice.getLabel() });
+  }
+  data.choiceListQuantity = choicelistQuantityData;
+  data.quantity = choicelistQuantityData[0].value;
+
+  // Are we editing an item from the cart? If so, populate our data based on the original item
+  var edit_parm = $sp.getParameter('edit');
+  data.is_cart_item = edit_parm == 'cart';
+
+  var cartName = data.is_cart_item ? 'DEFAULT' : 'saved_items';
+  var cart = new sn_sc.CartJS(cartName);
+  data.requestedFor = {value: gs.getUser().getID(), displayValue: gs.getUser().getDisplayName()};
+
+
+  if (edit_parm) {
+    var cart_item_id = $sp.getParameter("sys_id");
+    var gr = new GlideRecord("sc_cart_item");
+    if (!gr.get(cart_item_id) || gr.cart != cart.getCartID()) {
+      data.recordFound = false;
+      return;
+    }
+
+    var catItemData = {};
+    catItemData.sys_id = gr.getValue('cat_item');
+    catItemData.cart_item_id = gr.getUniqueValue();
+    catItemData.table = "sc_cart_item";
+    catItemData.is_ordering = true;
+
+    data.sc_cat_item = $sp.getCatalogItem(catItemData.sys_id);
+    data.sc_cat_item.isCartItem = true;
+    var values = getValues(cart_item_id);
+
+    for (var f in data.sc_cat_item._fields) {
+      // Put the values into the cat item fields
+      var field = data.sc_cat_item._fields[f];
+      if (typeof values[f] != "undefined" && typeof values[f].value != "undefined") {
+        if (values[f].type == 9 || values[f].type == 10)
+          field.value = values[f].displayValue;
+        else if (values[f].type == 25)
+          field.value = values[f].decrypted_value;
+        else
+          field.value = values[f].value;
+        field.displayValue = values[f].displayValue;
+        field.display_value_list = values[f].display_value_list;
+      }
+      updatePriceOnField(field);
+    }
+
+    data._generatedItemGUID = cart_item_id;
+    data.quantity = '' + gr.quantity;
+
+  } else {
+    data.sys_id = $sp.getParameter('sys_id');
+    data.sc_cat_item = $sp.getCatalogItem(data.sys_id, true);
+    data._generatedItemGUID = gs.generateGUID();
+  }
+
+  data._attachmentTable = 'sc_cart_item';
+
+
+  data.showPrices = $sp.showCatalogPrices();
+
+  data.maxAttachmentSize = parseInt(gs.getProperty('com.glide.attachment.max_size', 1024));
+  if (isNaN(data.maxAttachmentSize))
+    data.maxAttachmentSize = 24;
+
+  function getValues(sys_id) {
+    var qs = new sn_sc.VariablePoolQuestionSetJS();
+    qs.setCartID(sys_id);
+    qs.load();
+    var values = {};
+    var questions = qs.getFlatQuestions();
+    for (var i = 0; i < questions.length; i++) {
+      var qKey = questions[i].name;
+      if (typeof qKey == 'undefined' || qKey == '')
+        qKey = "IO:" + questions[i].sys_id;
+      values[qKey] = questions[i];
+    }
+    return values;
+  }
+
+  // Pricing utility functions
+  function setPrice(field, p, rp) {
+    if (p != undefined)
+      field.price = p;
+    if (rp != undefined)
+      field.recurring_price = rp;
+  }
+
+  function updatePriceForReferenceTable(field) {
+    var tableName = field.refTable + '';
+    if (tableName != undefined && tableName != null && tableName != '') {
+      var gr = new GlideRecord(tableName);
+      if (gr.isValid()) {
+        if (gr.get(field.value) && gr.isValidRecord()) {
+          updatePrice(gr, field);
+          updateRecurringPrice(gr, field);
+        }
+      }
+    }
+  }
+
+  function updatePriceForListCollector(field) {
+    var tableName = field.refTable + '';
+    if (tableName != undefined && tableName != null && tableName != '') {
+      var gr = new GlideRecord(tableName);
+      if (gr.isValid()) {
+        var values = field.value.split(',');
+        gr.addQuery('sys_id', values);
+        gr.query();
+        var p = 0.0;
+        var rp = 0.0;
+        var price_value_list = [];
+        while (gr.next()) {
+          var price_field = {};
+          updatePrice(gr, price_field);
+          updateRecurringPrice(gr, price_field);
+          if (price_field.price)
+            p += Number(price_field.price);
+          else
+            price_field.price = 0.0;
+          if (price_field.recurring_price)
+            rp += Number(price_field.recurring_price);
+          else
+            price_field.recurring_price = 0.0;
+          price_value_list.push(price_field);
+        }
+        field.price = p;
+        field.recurring_price = rp;
+        field.price_value_list = price_value_list;
+      }
+    }
+  }
+
+  function updatePrice(gr, field) {
+    if (gr.isValidField('price'))
+      field.price = gr.getValue('price');
+    else if (gr.isValidField('u_price'))
+      field.price = gr.getValue('u_price');
+  }
+
+  function updateRecurringPrice(gr, field) {
+    if (gr.isValidField('recurring_price'))
+      field.recurring_price = gr.getValue('recurring_price');
+    else if (gr.isValidField('u_recurring_price'))
+      field.recurring_price = gr.getValue('u_recurring_price');
+  }
+
+  function updatePriceOnField(field) {
+    if (field.type == 'boolean' || field.type == 'boolean_confirm') {
+      if (field.value == 'true' || field.value == true)
+        setPrice(field, field._pricing.price_if_checked, field._pricing.recurring_price_if_checked);
+      else
+        setPrice(field, 0, 0);
+    } else if (field.choices) {
+      field.choices.forEach(function (choice) {
+        if (choice.value + '' == field.value + '')
+          setPrice(field, choice.price, choice.recurring_price);
+      });
+    } else if (field._pricing && field._pricing.pricing_implications === true) {
+      if (field.type == 'reference')
+        updatePriceForReferenceTable(field);
+      else if (field.type == 'glide_list')
+        updatePriceForListCollector(field);
+    }
+  }
+})()
