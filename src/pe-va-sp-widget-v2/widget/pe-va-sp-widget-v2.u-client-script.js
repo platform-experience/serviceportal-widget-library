c7bf966623459b77e27c7dc93db3d6c34235dfa5
Sush <sush.chandrashekar@gmail.com>
2019-07-09 13:13:45 -0700

    Virtual Agent SP widget v2 (#138)

---
+++ src/pe-va-sp-widget-v2/widget/pe-va-sp-widget-v2.u-client-script.js
@@ -0,0 +1,81 @@
+function VirtualAgentController($log, $scope, $element, $document, spModal, $q, $timeout, $window) {
+	'use strict';
+
+	/* widget controller */
+	var $ctrl = this;
+	var $spContainer = $document.find('.sp-page-root');
+	var $widgetParent = $element.parent();
+
+	$ctrl.isWindowVisible = false;
+	$ctrl.hasUnreadMessages = false;
+	$ctrl.firstPress = false;
+	$ctrl.vaSource = '';
+
+
+	$ctrl.toggleWindow = function () {
+		if ($ctrl.isWindowVisible) {
+			$ctrl.isWindowVisible = false;
+			$timeout(function () {
+				if (!$ctrl.isWindowVisible) {
+					$element.find('.conversation-container').css("display", "none");
+					$element.find('.sn-connect-floating').css("display", "none");
+					$element.find('.sn-connect-floating-wrapper').css("display", "none");
+
+					// ios overlay hacky fix.
+					$document.find('.touch_scroll').css("-webkit-overflow-scrolling", "touch");
+				}
+			}, 300);
+		} else {
+			if (!$ctrl.firstPress) {
+				$ctrl.firstPress = true;
+				$ctrl.vaSource = '/$sn-va-web-client-app.do?sysparm_nostack=true&sysparm_stack=no';
+				if ($ctrl.options.va_url_params) {
+					$ctrl.vaSource = $ctrl.vaSource + '&' + $ctrl.options.va_url_params;
+				}
+			}
+			$ctrl.isWindowVisible = true;
+			$ctrl.hasUnreadMessages = false;
+			$element.find('.conversation-container').css("display", "block");
+			$element.find('.sn-connect-floating').css("display", "block");
+			$element.find('.sn-connect-floating-wrapper').css("display", "block");
+
+			// ios overlay hacky fix.
+			$document.find('.touch_scroll').css("-webkit-overflow-scrolling", "auto");
+		}
+	};
+
+	$window.addEventListener("message", function (e) {
+		if (e.data === 'sn-va-web-client-app-new-message' && $ctrl.isWindowVisible === false) {
+			$ctrl.hasUnreadMessages = true;
+		} else if (e.data === 'sn-va-web-client-app-trigger-login') {
+			$window.location.reload(true);
+		}
+	});
+
+	$element.find('.help-button').on("mouseup", function (e) {
+		e.target.blur();
+		e.stopPropagation();
+	});
+
+	$element.find('.help-icon').on("mouseup", function (e) {
+		e.target.blur();
+		e.stopPropagation();
+	});
+
+	$ctrl.openWindow = function () {
+		$ctrl.isWindowVisible = true;
+		// delay before clearing unread message indicator
+		// in-case user opens and closes quickly
+		$timeout(function () {
+			if ($ctrl.isWindowVisible) {
+				$ctrl.hasUnreadMessages = false;
+			}
+		}, 500);
+	};
+
+	var _closeWindow = function () {
+
+		$ctrl.isWindowVisible = false;
+		$ctrl.hasActiveConversation = false;
+	};
+}
---
+++ src/pe-va-sp-widget-v2/widget/pe-va-sp-widget-v2.u-server-script.js
@@ -0,0 +1,9 @@
+(function() {
+  /* populate the 'data' object */
+  /* e.g., data.table = $sp.getValue('table'); */
+
+	var instanceGR = $sp.getInstanceRecord();
+
+	options.va_url_params = options.va_url_params || "";
+	options.button_color = options.button_color || "red";
+})();
