0ab82d6a7ec35d84c4676720f2f402f57b0a28b0
claudilla <claudia.scruzr@gmail.com>
2019-10-02 11:20:07 -0700

    Created cases card widget (#132)

---
+++ src/pe-cases-card/widget/pe-cases-card.u-server-script.js
@@ -0,0 +1,21 @@
+(function() {
+	var table = options.src_table || 'sn_customerservice_case';
+	var grCases = new GlideRecord(table)
+	grCases.query();
+	data.listItems = [];
+	while(grCases.next()){
+		data.listItems.push({
+			number: grCases.number.getDisplayValue(),
+			short_description: grCases.short_description.getDisplayValue(),
+			state: grCases.state.getDisplayValue(),
+			owner:grCases.contact.getDisplayValue(),
+			created_on:grCases.sys_created_on.getDisplayValue(),
+			due_date: grCases.due_date.getDisplayValue(),
+			sys_id:grCases.getUniqueValue()
+		})		
+	}
+})();
+
+
+
+
