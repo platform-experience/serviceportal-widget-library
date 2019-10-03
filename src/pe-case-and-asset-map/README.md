5401efe9656fd556b7d29b25b4bce9d67dcc2380
Brian Nguyen <briant.nguyen@servicenow.com>
2019-10-03 18:26:05 -0500

    Case and asset map widget (#131)

---
+++ src/pe-case-and-asset-map/README.md
@@ -0,0 +1,41 @@
+# Case And Asset Map
+
+## Description
+
+This widget displays your cases and apps on a Google Maps like interface. You can click on each to view more detailed information about the location, as well as see the list of cases and assets, and click on them for more information. You can also click on an option to be redirected to Google Maps and have coordinate information automatically put in.
+
+## Screenshot
+
+![Case And Asset Map](https://raw.githubusercontent.com/platform-experience/serviceportal-widget-library/master/src/pe-case-and-asset-map/images/pe-case-and-asset-map.png)
+
+## Additional Information/Notes
+
+Best used if tested through Julie Lewis. Cases and Assets are tied to the user's account.
+
+## Installation
+
+Download and install update set **[pe-case-and-asset-map.u-update-set.xml](https://github.com/platform-experience/serviceportal-widget-library/blob/master/src/pe-case-and-asset-map/pe-case-and-asset-map.u-update-set.xml)**
+
+After installation, the widget can be accessed via the `Service Portal > Widgets` section for use and customization.
+
+- SN Product Documentation - ['Load a customization from a single XML file'](https://docs.servicenow.com/bundle/newyork-application-development/page/build/system-update-sets/task/t_SaveAnUpdateSetAsAnXMLFile.html)
+
+## Configuration
+
+> None
+
+## Platform Dependencies
+
+### SN System Tables
+
+> None
+
+### UI Dependencies
+
+> None
+
+## CSS/SASS Variables
+
+_CSS/SASS variables are given default values that can be overridden with theming or portal-level CSS._
+
+> None
---
+++ src/pe-case-and-asset-map/widget/pe-case-and-asset-map.u-server-script.js
@@ -0,0 +1,92 @@
+(function() {
+  data.apiKey = options.mapbox_apikey;
+  var currentUser = gs.getUserID();
+
+  var userAccount = '';
+  var user = new GlideRecord('customer_contact');
+  user.addQuery('sys_id', currentUser);
+  user.query();
+  if (user.next()) {
+    userAccount = user.account;
+  }
+  data.sites = [];
+  var locations = [];
+  data.list = [];
+  var grSites = new GlideRecord('sn_customerservice_case');
+  latLonQuery = 'asset.locationISNOTEMPTY^assetISNOTEMPTY^contact=' + currentUser; //'locationISNOTEMPTY^location.latitudeISNOTEMPTY^location.longitudeISNOTEMPTY';
+  grSites.addEncodedQuery(latLonQuery);
+  grSites.query();
+  while (grSites.next()) {
+    var locationExists = locations.indexOf(grSites.asset.location.getDisplayValue());
+    data.list.push({
+      sysId: grSites.sys_id.getDisplayValue(),
+      name: grSites.getDisplayValue(),
+      location: grSites.asset.location.getDisplayValue(),
+      street: grSites.asset.location.street.getDisplayValue(),
+      city: grSites.asset.location.city.getDisplayValue(),
+      state: grSites.asset.location.state.getDisplayValue(),
+      zip: grSites.asset.location.zip.getDisplayValue(),
+      lat: grSites.asset.location.latitude.getDisplayValue(),
+      lng: grSites.asset.location.longitude.getDisplayValue(),
+      types: 'Case'
+    });
+    if (locationExists == -1) {
+      locations.push(grSites.asset.location.getDisplayValue());
+      data.sites.push({
+        asset: grSites.asset.getDisplayValue(),
+        location: grSites.asset.location.getDisplayValue(),
+        sys_id: grSites.asset.location.sys_id,
+        street: grSites.asset.location.street.getDisplayValue(),
+        city: grSites.asset.location.city.getDisplayValue(),
+        state: grSites.asset.location.state.getDisplayValue(),
+        zip: grSites.asset.location.zip.getDisplayValue(),
+        phone: grSites.asset.location.phone.getDisplayValue(),
+        lat: grSites.asset.location.latitude.getDisplayValue(),
+        lng: grSites.asset.location.longitude.getDisplayValue(),
+        types: 'Case',
+        caseCount: 1,
+        assetCount: 0
+      });
+    } else {
+      data.sites[locationExists].caseCount++;
+    }
+  }
+
+  var grAssets = new GlideRecord('alm_asset');
+  latLonQuery = 'locationISNOTEMPTY^account=' + userAccount;
+  grAssets.addEncodedQuery(latLonQuery);
+  grAssets.query();
+  while (grAssets.next()) {
+    data.list.push({
+      name: grAssets.getDisplayValue(),
+      location: grAssets.location.getDisplayValue(),
+      street: grAssets.location.street.getDisplayValue(),
+      city: grAssets.location.city.getDisplayValue(),
+      state: grAssets.location.state.getDisplayValue(),
+      zip: grAssets.location.zip.getDisplayValue(),
+      lat: grAssets.location.latitude.getDisplayValue(),
+      lng: grAssets.location.longitude.getDisplayValue(),
+      types: 'Asset'
+    });
+    var assetLocationExists = locations.indexOf(grAssets.location.getDisplayValue());
+    if (assetLocationExists == -1) {
+      locations.push(grSites.asset.location.getDisplayValue());
+      data.sites.push({
+        asset: grAssets.getDisplayValue(),
+        sys_id: grAssets.location.sys_id,
+        location: grAssets.location.getDisplayValue(),
+        street: grAssets.location.street.getDisplayValue(),
+        city: grAssets.location.city.getDisplayValue(),
+        state: grAssets.location.state.getDisplayValue(),
+        zip: grAssets.location.zip.getDisplayValue(),
+        phone: grAssets.location.phone.getDisplayValue(),
+        lat: grAssets.location.latitude.getDisplayValue(),
+        lng: grAssets.location.longitude.getDisplayValue(),
+        types: 'Asset',
+        assetCount: 1
+      });
+    } else {
+      data.sites[assetLocationExists].assetCount++;
+    }
+  }
+})();
