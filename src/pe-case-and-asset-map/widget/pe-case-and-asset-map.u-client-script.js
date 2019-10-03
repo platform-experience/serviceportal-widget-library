3b1875d8554cc5dfa9ee2f2aa151b180d4cf6c20
stevengregory <sgregory@gmail.com>
2019-10-03 17:31:47 -0500

    Config, README and formatting updates

--- src/pe-case-and-asset-map/widget/pe-case-and-asset-map.u-client-script.js
+++ src/pe-case-and-asset-map/widget/pe-case-and-asset-map.u-client-script.js
@@ -1,280 +1,352 @@
-function MapPageController($scope, $rootScope, $window,$location, $anchorScroll, $timeout, leafletData, $q) {
-	/* widget controller */
-	var c = this;
-	$scope.firstFlip = true;
-	$location.hash('ff');
-	
-	
+function MapPageController(
+  $scope,
+  $rootScope,
+  $window,
+  $location,
+  $anchorScroll,
+  $timeout,
+  leafletData,
+  $q
+) {
+  var c = this;
+  $scope.firstFlip = true;
+  $location.hash('ff');
 
-	c.awesomeMarkerIconCurrentLocation = {
-		type: 'awesomeMarker',
-		icon: 'user',
-		markerColor: 'green',
-		prefix: 'fa',
-		iconColor: '#fff'
-	};
-	c.awesomeMarkerIconComputer = {
-		type: 'awesomeMarker',
-		icon: 'desktop',
-		markerColor: 'blue',
-		prefix: 'fa',
-		iconColor: '#fff'
-	};
-	c.awesomeMarkerIconConsumable = {
-		type: 'awesomeMarker',
-		icon: 'keyboard',
-		markerColor: 'blue',
-		prefix: 'fa',
-		iconColor: '#fff'
-	};
-	c.awesomeMarkerIconCase = {
-		type: 'awesomeMarker',
-		icon: 'exclamation-circle',
-		markerColor: 'blue',
-		prefix: 'fa',
-		iconColor: '#fff'
-	};
+  c.awesomeMarkerIconCurrentLocation = {
+    type: 'awesomeMarker',
+    icon: 'user',
+    markerColor: 'green',
+    prefix: 'fa',
+    iconColor: '#fff'
+  };
+  c.awesomeMarkerIconComputer = {
+    type: 'awesomeMarker',
+    icon: 'desktop',
+    markerColor: 'blue',
+    prefix: 'fa',
+    iconColor: '#fff'
+  };
+  c.awesomeMarkerIconConsumable = {
+    type: 'awesomeMarker',
+    icon: 'keyboard',
+    markerColor: 'blue',
+    prefix: 'fa',
+    iconColor: '#fff'
+  };
+  c.awesomeMarkerIconCase = {
+    type: 'awesomeMarker',
+    icon: 'exclamation-circle',
+    markerColor: 'blue',
+    prefix: 'fa',
+    iconColor: '#fff'
+  };
 
-	//Center of the USA
-	c.centerLocation = {
-		lat: 37.3382,
-		lng: 121.8863,
-		zoom: 3,
-		icon: c.awesomeMarkerIconCurrentLocation
-	}
-	c.startLocation = {
-		lat: 37.3382,
-		lng: 121.8863,
-		zoom: 3,
-		icon: c.awesomeMarkerIconCurrentLocation
-	}
-	function getDeviceLocation() {
-		var defer = $q.defer();
-		
-		if(navigator.geolocation && c.liveApp) {
-			navigator.geolocation.getCurrentPosition(function(location) {
-				c.centerLocation.lat = location.coords.latitude;
-				c.centerLocation.lng = location.coords.longitude;
-				c.startLocation.lat = location.coords.latitude;
-				c.startLocation.lng = location.coords.longitude;
-				if(location.coords.accuracy > 14){
-					c.centerLocation.zoom = Math.round(location.coords.accuracy*0.25);			
-					c.startLocation.zoom = Math.round(location.coords.accuracy*0.25);
-				}
-				defer.resolve();
-			});
-		}else{
-			//Default to New York
-			c.centerLocation = {
-				lat: 37.3382,
-				lng: -121.8863,
-				zoom: 9,
-				icon: c.awesomeMarkerIconCurrentLocation
-			};
-			angular.copy(c.centerLocation,c.startLocation);
-			defer.resolve();
-		}
-		return defer.promise;
-	}
+  //Center of the USA
+  c.centerLocation = {
+    lat: 37.3382,
+    lng: 121.8863,
+    zoom: 3,
+    icon: c.awesomeMarkerIconCurrentLocation
+  };
+  c.startLocation = {
+    lat: 37.3382,
+    lng: 121.8863,
+    zoom: 3,
+    icon: c.awesomeMarkerIconCurrentLocation
+  };
+  function getDeviceLocation() {
+    var defer = $q.defer();
 
-	c.mapbox = {
-		mapbox_layer: {
-			name: 'Mapbox',
-			
-			url: 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={apikey}',
-			type: 'xyz',
-			options: {
-				apikey: c.data.apiKey,
-				mapid: 'testing'
-			}
-		}
-	};
+    if (navigator.geolocation && c.liveApp) {
+      navigator.geolocation.getCurrentPosition(function(location) {
+        c.centerLocation.lat = location.coords.latitude;
+        c.centerLocation.lng = location.coords.longitude;
+        c.startLocation.lat = location.coords.latitude;
+        c.startLocation.lng = location.coords.longitude;
+        if (location.coords.accuracy > 14) {
+          c.centerLocation.zoom = Math.round(location.coords.accuracy * 0.25);
+          c.startLocation.zoom = Math.round(location.coords.accuracy * 0.25);
+        }
+        defer.resolve();
+      });
+    } else {
+      //Default to New York
+      c.centerLocation = {
+        lat: 37.3382,
+        lng: -121.8863,
+        zoom: 9,
+        icon: c.awesomeMarkerIconCurrentLocation
+      };
+      angular.copy(c.centerLocation, c.startLocation);
+      defer.resolve();
+    }
+    return defer.promise;
+  }
 
-	c.$onInit = function() {
-		$rootScope.$broadcast('refreshFooterUrl');
+  c.mapbox = {
+    mapbox_layer: {
+      name: 'Mapbox',
 
-		c.loading = true;
-		c.liveApp = false;
-		c.masterMarkers = [];
-		c.markers = [];	
-		c.masterList = [];
-		c.list = []
-		c.searchObject = $location.search();
+      url:
+        'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={apikey}',
+      type: 'xyz',
+      options: {
+        apikey: c.data.apiKey,
+        mapid: 'testing'
+      }
+    }
+  };
 
-		c.tile = c.mapbox.mapbox_layer;	
-		getDeviceLocation().then(function(){
-			$timeout(function() {
-				demoMarkers();
-				c.loading = false;	
-			}, 3000);
-		});
-	};
-	
-	$scope.$watch("c.loading", function(value) {
-		if (value === false) {
-			leafletData.getMap().then(function(map) {
-				$timeout(function() {
-					map.invalidateSize();
-				}, 300);
-			});
-		}
-	});
+  c.$onInit = function() {
+    $rootScope.$broadcast('refreshFooterUrl');
 
-	function demoMarkers(){
-		var list = [];
-		for(var j = 0; j < c.data.list.length; j++){
-			list[j] = {
-				name: c.data.list[j].name,
-				address: c.data.list[j].street +' ' + c.data.list[j].city +', '+ c.data.list[j].state +' '+ c.data.list[j].zip,
-				types: c.data.list[j].types,
-				lat: c.data.list[j].lat,
-				lng: c.data.list[j].lng,
-				sysId: c.data.list[j].sysId,
-				location: c.data.list[j].location
-			};
+    c.loading = true;
+    c.liveApp = false;
+    c.masterMarkers = [];
+    c.markers = [];
+    c.masterList = [];
+    c.list = [];
+    c.searchObject = $location.search();
 
-		}
-		var markers = [];
-		for(var i = 0; i < c.data.sites.length; i++){
-			markers[i] = {
-				name: '',
-				icon: c.awesomeMarkerIconBank,
-				message : '',
-				image : '',
-				lat: parseFloat(c.data.sites[i].lat),
-				lng: parseFloat(c.data.sites[i].lng),
-				getMessageScope: function(){ return $scope; }
-			};
-			
-			var msg = [];
-			msg.push('<div class="flex-it">');
-			if(c.data.sites[i].types.indexOf('Case')>-1){
-				if(c.data.sites[i].caseCount == 1){
-				markers[i].name = c.data.sites[i].caseCount + ' Case';
-				} else{
-					markers[i].name = c.data.sites[i].caseCount + ' Cases';
-				}
-				
-				markers[i].icon = c.awesomeMarkerIconCase;
-				markers[i].type = 'Case';
-				markers[i].address = c.data.sites[i].street +' ' + c.data.sites[i].city +', '+ c.data.sites[i].state +' '+ c.data.sites[i].zip;
+    c.tile = c.mapbox.mapbox_layer;
+    getDeviceLocation().then(function() {
+      $timeout(function() {
+        demoMarkers();
+        c.loading = false;
+      }, 3000);
+    });
+  };
 
-					msg.push('<div><div class="info-title"> # of Cases: '+ c.data.sites[i].caseCount +'</div>');
-				msg.push('<div><div class="info-title"> # of my Assets: '+ c.data.sites[i].assetCount +'</div>');
-				msg.push('<div class="info">'+ c.data.sites[i].street +'</div>');
-				msg.push('<div class="info">'+ c.data.sites[i].city +', '+ c.data.sites[i].state +' '+ c.data.sites[i].zip +'</div>');
-				msg.push("<div class='redirect' ng-click='c.flipFilter(" + '"'+ c.data.sites[i].location+ '"' + ',' + '"' + markers[i].type + '"' + ")'> See the list of Cases</div>");
-				msg.push("<div class='redirect' ng-click='c.flipFilter(" + '"'+ c.data.sites[i].location+ '"' + ', "Asset"'+ ")'> See the list of Assets</div>");
+  $scope.$watch('c.loading', function(value) {
+    if (value === false) {
+      leafletData.getMap().then(function(map) {
+        $timeout(function() {
+          map.invalidateSize();
+        }, 300);
+      });
+    }
+  });
 
-			}else	if(c.data.sites[i].types.indexOf('Asset')>-1){
-			
-				
-				markers[i].icon = c.awesomeMarkerIconComputer;
-				markers[i].name = c.data.sites[i].asset;
-				markers[i].type = 'Asset';
-				markers[i].address = c.data.sites[i].street +' ' + c.data.sites[i].city +', '+ c.data.sites[i].state +' '+ c.data.sites[i].zip;
-				msg.push('<div><div class="info-title"> # of my Assets: '+ c.data.sites[i].assetCount +'</div>');
-				msg.push('<div class="info">'+ c.data.sites[i].street +'</div>');
-				msg.push('<div class="info">'+ c.data.sites[i].city +', '+ c.data.sites[i].state +' '+ c.data.sites[i].zip +'</div>');
-				msg.push("<div class='redirect' ng-click='c.flipFilter(" + '"'+ c.data.sites[i].location+ '"' + ',' + '"' + markers[i].type + '"' + ")'> See the list of Assets</div>");
-			}
-			msg.push('</div>');
-			markers[i].message = msg.join('');
-		}
-		
-		markers[c.data.sites.length] = {
-				name: '',
-				icon: c.startLocation.icon,
-				message : 'You are Here!',
-				image : '',
-				lat: c.startLocation.lat,
-				lng: c.startLocation.lng
-			};
-		angular.copy(markers, c.markers);
-		angular.copy(list, c.list);
-		angular.copy(c.markers, c.masterMarkers);
-		angular.copy(c.list, c.masterList);
-	}
+  function demoMarkers() {
+    var list = [];
+    for (var j = 0; j < c.data.list.length; j++) {
+      list[j] = {
+        name: c.data.list[j].name,
+        address:
+          c.data.list[j].street +
+          ' ' +
+          c.data.list[j].city +
+          ', ' +
+          c.data.list[j].state +
+          ' ' +
+          c.data.list[j].zip,
+        types: c.data.list[j].types,
+        lat: c.data.list[j].lat,
+        lng: c.data.list[j].lng,
+        sysId: c.data.list[j].sysId,
+        location: c.data.list[j].location
+      };
+    }
+    var markers = [];
+    for (var i = 0; i < c.data.sites.length; i++) {
+      markers[i] = {
+        name: '',
+        icon: c.awesomeMarkerIconBank,
+        message: '',
+        image: '',
+        lat: parseFloat(c.data.sites[i].lat),
+        lng: parseFloat(c.data.sites[i].lng),
+        getMessageScope: function() {
+          return $scope;
+        }
+      };
 
+      var msg = [];
+      msg.push('<div class="flex-it">');
+      if (c.data.sites[i].types.indexOf('Case') > -1) {
+        if (c.data.sites[i].caseCount == 1) {
+          markers[i].name = c.data.sites[i].caseCount + ' Case';
+        } else {
+          markers[i].name = c.data.sites[i].caseCount + ' Cases';
+        }
 
-	function insertMarkers(markers) {
+        markers[i].icon = c.awesomeMarkerIconCase;
+        markers[i].type = 'Case';
+        markers[i].address =
+          c.data.sites[i].street +
+          ' ' +
+          c.data.sites[i].city +
+          ', ' +
+          c.data.sites[i].state +
+          ' ' +
+          c.data.sites[i].zip;
 
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].message = '<div class="flex-it">';
-			if (markers[i].type == 'ATM') {
-				markers[i].icon = c.awesomeMarkerIconComputer;
-				markers[i].message = markers[i].message; 
+        msg.push(
+          '<div><div class="info-title"> # of Cases: ' + c.data.sites[i].caseCount + '</div>'
+        );
+        msg.push(
+          '<div><div class="info-title"> # of my Assets: ' + c.data.sites[i].assetCount + '</div>'
+        );
+        msg.push('<div class="info">' + c.data.sites[i].street + '</div>');
+        msg.push(
+          '<div class="info">' +
+            c.data.sites[i].city +
+            ', ' +
+            c.data.sites[i].state +
+            ' ' +
+            c.data.sites[i].zip +
+            '</div>'
+        );
+        msg.push(
+          '<div class=\'redirect\' ng-click=\'c.flipFilter(' +
+            '"' +
+            c.data.sites[i].location +
+            '"' +
+            ',' +
+            '"' +
+            markers[i].type +
+            '"' +
+            ')\'> See the list of Cases</div>'
+        );
+        msg.push(
+          '<div class=\'redirect\' ng-click=\'c.flipFilter(' +
+            '"' +
+            c.data.sites[i].location +
+            '"' +
+            ', "Asset"' +
+            ')\'> See the list of Assets</div>'
+        );
+      } else if (c.data.sites[i].types.indexOf('Asset') > -1) {
+        markers[i].icon = c.awesomeMarkerIconComputer;
+        markers[i].name = c.data.sites[i].asset;
+        markers[i].type = 'Asset';
+        markers[i].address =
+          c.data.sites[i].street +
+          ' ' +
+          c.data.sites[i].city +
+          ', ' +
+          c.data.sites[i].state +
+          ' ' +
+          c.data.sites[i].zip;
+        msg.push(
+          '<div><div class="info-title"> # of my Assets: ' + c.data.sites[i].assetCount + '</div>'
+        );
+        msg.push('<div class="info">' + c.data.sites[i].street + '</div>');
+        msg.push(
+          '<div class="info">' +
+            c.data.sites[i].city +
+            ', ' +
+            c.data.sites[i].state +
+            ' ' +
+            c.data.sites[i].zip +
+            '</div>'
+        );
+        msg.push(
+          '<div class=\'redirect\' ng-click=\'c.flipFilter(' +
+            '"' +
+            c.data.sites[i].location +
+            '"' +
+            ',' +
+            '"' +
+            markers[i].type +
+            '"' +
+            ')\'> See the list of Assets</div>'
+        );
+      }
+      msg.push('</div>');
+      markers[i].message = msg.join('');
+    }
 
-			} else if (markers[i].type == 'Bank') {
-				markers[i].name = markers[i].name + " - Branch"
-				markers[i].icon = c.awesomeMarkerIconBank;
-				markers[i].message = markers[i].message; 
-			} else {
-				markers[i].name = '';
-				markers[i].message = markers[i].message + 'You are Here!';
-				markers[i].image = '';
-			}
-			markers[i].message = markers[i].message + '<div><div class="info-title">'+ markers[i].name  +'</div>';
-			markers[i].message = markers[i].message + '<div class="info">M-F: 9AM - 5PM</div>';
-			markers[i].message = markers[i].message + '<div class="info">SAT: 9AM - 3PM</div>';
-			markers[i].message = markers[i].message + '</div></div>';
-		}
-		return markers;
-	}
+    markers[c.data.sites.length] = {
+      name: '',
+      icon: c.startLocation.icon,
+      message: 'You are Here!',
+      image: '',
+      lat: c.startLocation.lat,
+      lng: c.startLocation.lng
+    };
+    angular.copy(markers, c.markers);
+    angular.copy(list, c.list);
+    angular.copy(c.markers, c.masterMarkers);
+    angular.copy(c.list, c.masterList);
+  }
 
-	c.filterMarkers = function(type) {
-		var tempArray = [];
-		var listArray = [];
-		if(type == 'Case and Asset'){
-			for(var j = 0; j < c.masterMarkers.length; j++){
-				tempArray.push(c.masterMarkers[j]);
-				
-			}
-			for(var k = 0; k < c.masterList.length; k++){
-				listArray.push(c.masterList[k]);
-			}
-		} else{
-			for (var i = 0; i < c.masterMarkers.length; i++) {
-				if (c.masterMarkers[i].type && c.masterMarkers[i].type.indexOf(type)>-1) {
-					tempArray.push(c.masterMarkers[i]);
-				}
-			}
-			for(var x = 0; x < c.masterList.length; x++){
-				if(c.masterList[x].types == type){
-					listArray.push(c.masterList[x]);
-				}
-			}
-		}
-		c.markers = tempArray;
-		c.list = listArray;
-		c.list.splice(0,0,c.startLocation);
-		c.markers.splice(0, 0, c.startLocation);
-	}
+  function insertMarkers(markers) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].message = '<div class="flex-it">';
+      if (markers[i].type == 'ATM') {
+        markers[i].icon = c.awesomeMarkerIconComputer;
+        markers[i].message = markers[i].message;
+      } else if (markers[i].type == 'Bank') {
+        markers[i].name = markers[i].name + ' - Branch';
+        markers[i].icon = c.awesomeMarkerIconBank;
+        markers[i].message = markers[i].message;
+      } else {
+        markers[i].name = '';
+        markers[i].message = markers[i].message + 'You are Here!';
+        markers[i].image = '';
+      }
+      markers[i].message =
+        markers[i].message + '<div><div class="info-title">' + markers[i].name + '</div>';
+      markers[i].message = markers[i].message + '<div class="info">M-F: 9AM - 5PM</div>';
+      markers[i].message = markers[i].message + '<div class="info">SAT: 9AM - 3PM</div>';
+      markers[i].message = markers[i].message + '</div></div>';
+    }
+    return markers;
+  }
 
-	c.flipIt = function(){
-		// Need to force the moving of the page to the top 
-		// because of scrolling on the back-part page
-		$anchorScroll();
-		$scope.flip();
-		c.map = !c.map;
-	}
-	c.flipFilter = function(location, type){
-		// Need to force the moving of the page to the top 
-		// because of scrolling on the back-part page
-		var listArray = []
-		for(var i = 0; i < c.masterList.length; i++){
-				if(c.masterList[i].location == location && c.masterList[i].types == type){
-					listArray.push(c.masterList[i]);
-				}
-			}
-		c.list = listArray;
-		$anchorScroll();
-		$scope.flip();
-		c.map = !c.map;
-	}
-	$scope.$on('leafletDirectiveMarker.click', function(e, args) {
-		var marker = args.leafletObject.getLatLng();
-		c.centerLocation.lat = marker.lat + 0.002;
-		c.centerLocation.lng = marker.lng;
-	});
+  c.filterMarkers = function(type) {
+    var tempArray = [];
+    var listArray = [];
+    if (type == 'Case and Asset') {
+      for (var j = 0; j < c.masterMarkers.length; j++) {
+        tempArray.push(c.masterMarkers[j]);
+      }
+      for (var k = 0; k < c.masterList.length; k++) {
+        listArray.push(c.masterList[k]);
+      }
+    } else {
+      for (var i = 0; i < c.masterMarkers.length; i++) {
+        if (c.masterMarkers[i].type && c.masterMarkers[i].type.indexOf(type) > -1) {
+          tempArray.push(c.masterMarkers[i]);
+        }
+      }
+      for (var x = 0; x < c.masterList.length; x++) {
+        if (c.masterList[x].types == type) {
+          listArray.push(c.masterList[x]);
+        }
+      }
+    }
+    c.markers = tempArray;
+    c.list = listArray;
+    c.list.splice(0, 0, c.startLocation);
+    c.markers.splice(0, 0, c.startLocation);
+  };
+
+  c.flipIt = function() {
+    // Need to force the moving of the page to the top
+    // because of scrolling on the back-part page
+    $anchorScroll();
+    $scope.flip();
+    c.map = !c.map;
+  };
+  c.flipFilter = function(location, type) {
+    // Need to force the moving of the page to the top
+    // because of scrolling on the back-part page
+    var listArray = [];
+    for (var i = 0; i < c.masterList.length; i++) {
+      if (c.masterList[i].location == location && c.masterList[i].types == type) {
+        listArray.push(c.masterList[i]);
+      }
+    }
+    c.list = listArray;
+    $anchorScroll();
+    $scope.flip();
+    c.map = !c.map;
+  };
+  $scope.$on('leafletDirectiveMarker.click', function(e, args) {
+    var marker = args.leafletObject.getLatLng();
+    c.centerLocation.lat = marker.lat + 0.002;
+    c.centerLocation.lng = marker.lng;
+  });
 }
